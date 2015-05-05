2cd91c3f17bf31b1b1e3119d4785e26618c2d797
unknown <wieserh@hertz-a.inet.dkfz-heidelberg.de>
2015-05-05 13:02:59 +0200

    added optimization of RBE x dose

--- matRad_bioObjFuncRBExD.m
+++ matRad_bioObjFuncRBExD.m
@@ -1,6 +1,6 @@
-function [f, g] = matRad_bioObjFunc(w,dij,cst)
+function [f, g] = matRad_bioObjFuncRBExD(w,dij,cst)
 % %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% matRad optimization function based on the biologcial effect
+% matRad optimization function based on the biological effecitve dose
 % 
 % call
 %   [f, g] = matRad_bioObjFunc(w,dij,cst)
@@ -15,7 +15,7 @@ function [f, g] = matRad_bioObjFunc(w,dij,cst)
 %   g: gradient vector
 %
 % References
-%   http://iopscience.iop.org/0031-9155/51/12/009
+%   http://iopscience.iop.org/0031-9155/51/8/001
 %
 % %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %
@@ -42,13 +42,19 @@ function [f, g] = matRad_bioObjFunc(w,dij,cst)
 % %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 
+% Numbers of voxels
+numVoxels = size(dij.physicalDose,1);
+
 % calculate biological effect
 linTerm  = dij.mAlphaDose*w;
 quadTerm = dij.mSqrtBetaDose*w;
 e = linTerm + quadTerm.^2;
 
-% Numbers of voxels
-numVoxels = size(dij.physicalDose,1);
+% calculate RBX x dose
+ScaledEffect = (e./dij.bx)+(dij.gamma.^2);
+[idx,~] = find(~isnan(ScaledEffect));
+ScaledEffect(idx)=sqrt(ScaledEffect(idx));
+RBExD = ScaledEffect-dij.gamma;
 
 % Initializes f
 f = 0;
@@ -66,22 +72,19 @@ for  i = 1:size(cst,1)
     % Only take OAR or target VOI.
     if isequal(cst{i,3},'OAR') || isequal(cst{i,3},'TARGET')
         
-        % get effect vector in current VOI
-        e_i = e(cst{i,4});
-        
+        % get rbe x dose vector in current VOI
+        RBExD_i = RBExD(cst{i,4});
+         
         % loop over the number of constraints for the current VOI
         for j = 1:size(cst{i,6},2)
             
             % get Penalty
             rho = cst{i,6}(j).parameter(1);
             
-            % refernce effect
-            e_ref = dij.ax(cst{i,4}).*cst{i,6}(j).parameter(2)+dij.bx(cst{i,4})*cst{i,6}(j).parameter(2)^2;
-            
             if isequal(cst{i,6}(j).type, 'square underdosing')
   
                 % underdose : effect minus reference effect
-                underdose = e_i - e_ref;
+                underdose = RBExD_i - cst{i,6}(j).parameter(2);
 
                 % apply positive operator
                 underdose(underdose>0) = 0;
@@ -95,7 +98,7 @@ for  i = 1:size(cst,1)
             elseif isequal(cst{i,6}(j).type, 'square overdosing')
                 
                 % overdose : Dose minus prefered dose
-                overdose = e_i - e_ref;
+                overdose = RBExD_i - cst{i,6}(j).parameter(2);
                 
                 % apply positive operator
                 overdose(overdose<0) = 0;
@@ -109,24 +112,24 @@ for  i = 1:size(cst,1)
             elseif isequal(cst{i,6}(j).type, 'square deviation')
                 
                 % deviation : Dose minus prefered dose
-                deviation = e_i - e_ref;
+                deviation = RBExD_i - cst{i,6}(j).parameter(2);
                 
                 % claculate objective function
                 f = f + (rho/size(cst{i,4},1))*(deviation'*deviation);
                 
                 % calculate delta
                 delta_deviation(cst{i,4}) = delta_deviation(cst{i,4}) + (rho/size(cst{i,4},1))*deviation;
-            
+               
             elseif isequal(cst{i,6}(j).type, 'mean')              
                 
                 % calculate objective function
-                f = f + (rho/size(cst{i,4},1))*sum(e_i);
+                f = f + (rho/size(cst{i,4},1))*sum(RBExD_i);
                 
                 % calculate delta
                 delta_mean(cst{i,4}) = delta_mean(cst{i,4}) + ...
                     (rho/size(cst{i,4},1))*ones(size(cst{i,4},1),1);
                 
-             elseif isequal(cst{i,6}(j).type, 'EUD') 
+            elseif isequal(cst{i,6}(j).type, 'EUD') 
                 
                 % get exponent for EUD
                 exponent = cst{i,6}(j).exponent;
@@ -134,12 +137,12 @@ for  i = 1:size(cst,1)
                 % calculate objective function and delta
                 if sum(e_i.^exponent)>0
                     
-                    f = f + rho*nthroot((1/size(cst{i,4},1))*sum(e_i.^exponent),exponent);
+                    f = f + rho*nthroot((1/size(cst{i,4},1))*sum(RBExD_i.^exponent),exponent);
                     
                     delta_EUD(cst{i,4}) = delta_EUD(cst{i,4}) + ...
-                        rho*nthroot(1/size(cst{i,4},1),exponent) * sum(e_i.^exponent)^((1-exponent)/exponent) * (e_i.^(exponent-1));                    
+                        rho*nthroot(1/size(cst{i,4},1),exponent) * sum(RBExD_i.^exponent)^((1-exponent)/exponent) * (RBExD_i.^(exponent-1));                    
                 end    
-                   
+                    
             else
                 
                 error('undefined objective in cst struct');
@@ -154,18 +157,9 @@ end
 % gradient calculation
 if nargout > 1
     delta = delta_underdose + delta_overdose + delta_deviation + ...
-                delta_mean + delta_EUD;
+                delta_mean + delta_EUD;        
+    delta = delta./(dij.bx.*ScaledEffect);
     vBias= (delta' * dij.mAlphaDose)';
-    mPsi = ((delta.*quadTerm)'*dij.physicalDose)';
+    mPsi = ((2*delta.*quadTerm)'*dij.physicalDose)';
     g = 2*(vBias+mPsi);    
-    
-    
-    mPsi1 = (quadTerm'*dij.physicalDose);
-    
-    % first gradient - according to Paper of Jan Wilkens - Fast Multifield
-    NumGrad = 1;  % determines which gradient should be calculated
-    vBias = dij.mAlphaDose(:,NumGrad);
-    vPsi  = 2*(dij.mSqrtBetaDose(:,NumGrad).*(dij.physicalDose*w));
-    g_single = 2*((delta'*(vBias+vPsi)));
-    fprintf(['first gradient jan wilkens' num2str(g_single) '\n']);
 end
