c1e4dbf793ac0427065054ebb8007ccbee5a635b
unknown <bangertm@Michelson.inet.dkfz-heidelberg.de>
2016-02-11 14:51:23 +0100

    Revert "* Change License agreement to Eclipse Public License 1.0"
    This reverts commit 13dd26672a317780d0bd9881f593af37485c09c4.

--- matRad_bioObjFuncRBExD.m
+++ matRad_bioObjFuncRBExD.m
@@ -18,26 +18,29 @@ function [f, g] = matRad_bioObjFuncRBExD(w,dij,cst)
 %   [1] http://iopscience.iop.org/0031-9155/51/8/001
 %
 % %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %
-% Copyright 2016, Mark Bangert, on behalf of the matRad development team
+% Copyright 2015, Mark Bangert, on behalf of the matRad development team
 %
 % m.bangert@dkfz.de
 %
 % This file is part of matRad.
 %
 % matrad is free software: you can redistribute it and/or modify it under 
-% the terms of the Eclipse Public License 1.0 (EPL-1.0).
+% the terms of the GNU General Public License as published by the Free 
+% Software Foundation, either version 3 of the License, or (at your option)
+% any later version.
 %
 % matRad is distributed in the hope that it will be useful, but WITHOUT ANY
 % WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
-% FOR A PARTICULAR PURPOSE.
+% FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+% details.
 %
-% You should have received a copy of the EPL-1.0 in the file license.txt
-% along with matRad. If not, see <http://opensource.org/licenses/EPL-1.0>.
+% You should have received a copy of the GNU General Public License in the
+% file license.txt along with matRad. If not, see
+% <http://www.gnu.org/licenses/>.
 %
 % %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
 
 % Numbers of voxels
 numVoxels = size(dij.physicalDose,1);
--- matRad_projectedLBFGS.m
+++ matRad_projectedLBFGS.m
@@ -23,21 +23,25 @@ function wOpt = matRad_projectedLBFGS(objFunc,projFunc,wInit,visBool,varargin)
 
 % %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %
-% Copyright 2016, Mark Bangert, on behalf of the matRad development team
+% Copyright 2015, Mark Bangert, on behalf of the matRad development team
 %
 % m.bangert@dkfz.de
 %
 % This file is part of matRad.
 %
 % matrad is free software: you can redistribute it and/or modify it under 
-% the terms of the Eclipse Public License 1.0 (EPL-1.0).
+% the terms of the GNU General Public License as published by the Free 
+% Software Foundation, either version 3 of the License, or (at your option)
+% any later version.
 %
 % matRad is distributed in the hope that it will be useful, but WITHOUT ANY
 % WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
-% FOR A PARTICULAR PURPOSE.
+% FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+% details.
 %
-% You should have received a copy of the EPL-1.0 in the file license.txt
-% along with matRad. If not, see <http://opensource.org/licenses/EPL-1.0>.
+% You should have received a copy of the GNU General Public License in the
+% file license.txt along with matRad. If not, see
+% <http://www.gnu.org/licenses/>.
 %
 % %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
