5de1993e4b9887a29bae7f00ab81c3e6b7e8b1f6
unknown <mescher@gauss.inet.dkfz-heidelberg.de>
2016-04-08 17:17:13 +0200

    * add function that calculates surface voxel of VOI

---
+++ matRad_getVOISurfaceVoxel.m
@@ -0,0 +1,78 @@
+function VOISurfaceVoxel = matRad_getVOISurfaceVoxel(cst,VOIName)
+
+% get cst index
+cstidx = find(~cellfun('isempty',strfind(cst(:,2),VOIName)));
+
+% get surface voxels of TARGET
+[xCoords_vox, yCoords_vox, zCoords_vox] = meshgrid(1:ct.cubeDim(1),1:ct.cubeDim(2),1:ct.cubeDim(3));
+
+xCoords = xCoords_vox(:);
+yCoords = yCoords_vox(:);
+zCoords = zCoords_vox(:);
+
+xCoordsVOI= xCoords(cst{cstidx ,4}{1});
+yCoordsVOI = yCoords(cst{cstidx ,4}{1});
+zCoordsVOI = zCoords(cst{cstidx ,4}{1});
+
+xCoordsVOIUnique = unique(xCoordsVOI); 
+yCoordsVOIUnique = unique(yCoordsVOI); 
+zCoordsVOIUnique = unique(zCoordsVOI); 
+
+% x
+VOISurfaceX = [];
+for i = 1:length(xCoordsVOIUnique)
+    
+    idx = find(xCoordsVOI == xCoordsVOIUnique(i));
+    
+    if i ~= 1 && i ~= length(xCoordsVOIUnique)
+        idx = xCoordsVOI == xCoordsVOIUnique(i) & yCoordsVOI == min(yCoordsVOI(idx)) |...
+              xCoordsVOI == xCoordsVOIUnique(i) & yCoordsVOI == max(yCoordsVOI(idx)) |...
+              xCoordsVOI == xCoordsVOIUnique(i) & zCoordsVOI == min(zCoordsVOI(idx)) |...
+              xCoordsVOI == xCoordsVOIUnique(i) & zCoordsVOI == max(zCoordsVOI(idx));
+
+        idx = find(idx);  
+    end
+    
+    VOISurfaceX = [VOISurfaceX;cst{cstidx,4}{1}(idx)];   
+end
+
+% y
+VOISurfaceY = [];
+for i = 1:length(yCoordsVOIUnique)
+    
+    idx = find(yCoordsVOI == yCoordsVOIUnique(i));
+    
+    if i ~= 1 && i ~= length(yCoordsVOIUnique)
+        idx = yCoordsVOI == yCoordsVOIUnique(i) & xCoordsVOI == min(xCoordsVOI(idx)) |...
+              yCoordsVOI == yCoordsVOIUnique(i) & xCoordsVOI == max(xCoordsVOI(idx)) |...
+              yCoordsVOI == yCoordsVOIUnique(i) & zCoordsVOI == min(zCoordsVOI(idx)) |...
+              yCoordsVOI == yCoordsVOIUnique(i) & zCoordsVOI == max(zCoordsVOI(idx));
+
+        idx = find(idx);  
+    end
+    
+    VOISurfaceY = [VOISurfaceY;cst{cstidx,4}{1}(idx)];   
+end
+
+% z
+VOISurfaceZ = [];
+for i = 1:length(zCoordsVOIUnique)
+    
+    idx = find(zCoordsVOI == zCoordsVOIUnique(i));
+    
+    if i ~= 1 && i ~= length(zCoordsVOIUnique)
+        idx = zCoordsVOI == zCoordsVOIUnique(i) & yCoordsVOI == min(yCoordsVOI(idx)) |...
+              zCoordsVOI == zCoordsVOIUnique(i) & yCoordsVOI == max(yCoordsVOI(idx)) |...
+              zCoordsVOI == zCoordsVOIUnique(i) & xCoordsVOI == min(xCoordsVOI(idx)) |...
+              zCoordsVOI == zCoordsVOIUnique(i) & xCoordsVOI == max(xCoordsVOI(idx));
+
+        idx = find(idx);  
+    end
+    
+    VOISurfaceZ = [VOISurfaceZ;cst{cstidx,4}{1}(idx)];   
+end
+
+VOISurfaceVoxel = union(VOISurfaceX,VOISurfaceY);
+VOISurfaceVoxel = union(VOISurfaceVoxel,VOISurfaceZ); 
+
+end
