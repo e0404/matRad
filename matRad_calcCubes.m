6d1c1de90ba51c32e286bde84ea18b1768592c0b
Niklas Wahl <n.wahl@dkfz.de>
2016-05-13 17:24:18 +0200

    Visualization of the Overlap Priorities on the CT

--- matRad_calcCubes.m
+++ matRad_calcCubes.m
@@ -42,6 +42,12 @@ resultGUI.w = w;
 % calc dose and reshape from 1D vector to 2D array
 resultGUI.physicalDose = reshape(dij.physicalDose{scenNum}*resultGUI.w,dij.dimensions);
 
+%Calculate Overlap Cube
+resultGUI.overlapCube = zeros(size(resultGUI.physicalDose));
+for i = 1:size(cst,1)
+   resultGUI.overlapCube(cst{i,4}{1}) = cst{i,5}.Priority;
+end 
+
 if isfield(dij,'mAlphaDose') && isfield(dij,'mSqrtBetaDose')
 
     a_x = zeros(size(resultGUI.physicalDose));
