22d2d0ff86c8543d90a22f2408ca494663dc5d73
unknown <mescher@gauss.inet.dkfz-heidelberg.de>
2016-06-02 13:12:37 +0200

    *improve dch calculion

--- matRad_calcDCH.m
+++ matRad_calcDCH.m
@@ -1,27 +1,26 @@
 function [dchPoints,coverageProbabilities] = matRad_calcDCH(volume,doseVec,cst,numOfScenarios,dij)
 
-% set dose points in DCH 
-n         = 10000;
-dchPoints = linspace(0,max(vertcat(doseVec{:}))*1.05,n);
-
-% calculate inverse DVH in every scenario and the deviation from dchPoints
+% calculate inverse DVH in every scenario
 if length(doseVec) > 1
     % use dij scenarios
     for Scen = 1:numOfScenarios
-        doseInverseDVH = matRad_calcInversDVH(volume/100,doseVec{Scen}(cst{1,4}{1}));
-        dev(Scen,:)    = doseInverseDVH - dchPoints;
+        doseInverseDVH(Scen) = matRad_calcInversDVH(volume/100,doseVec{Scen}(cst{1,4}{1}));
     end
 elseif length(doseVec) == 1    
     % create scenarios with shifts
     for Scen = 1:numOfScenarios
-        idxShift       = cst{1,5}.voxelShift(2,Scen) + cst{1,5}.voxelShift(1,Scen)*dij.dimensions(1) + cst{1,5}.voxelShift(3,Scen)*dij.dimensions(2)*dij.dimensions(1);
-        doseInverseDVH = matRad_calcInversDVH(volume/100,doseVec{1}(cst{1,4}{1}-idxShift));
-        dev(Scen,:)    = doseInverseDVH - dchPoints;
+        idxShift             = cst{1,5}.voxelShift(2,Scen) + cst{1,5}.voxelShift(1,Scen)*dij.dimensions(1) + cst{1,5}.voxelShift(3,Scen)*dij.dimensions(2)*dij.dimensions(1);
+        doseInverseDVH(Scen) = matRad_calcInversDVH(volume/100,doseVec{1}(cst{1,4}{1}-idxShift));
     end
 end
 
+% set dose points in dch
+% dchPoints = [0,sort(doseInverseDVH),max(vertcat(doseVec{:}))*1.05];
+dchPoints = linspace(0,max(vertcat(doseVec{:}))*1.05,10000);
+
 % calculate coverage probability in every dchPoint by counting number of 
-% scenarios with doseInverseDVH >= dchPoint <-> dev >= 0
-coverageProbabilities = (1/numOfScenarios)*sum(dev >= 0)*100;
+% scenarios with doseInverseDVH >= dchPoint
+logicalDoseMask       = bsxfun(@ge,doseInverseDVH',dchPoints);
+coverageProbabilities = (1/numOfScenarios)*sum(logicalDoseMask)*100;
 
 end
