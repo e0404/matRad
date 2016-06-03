054e03605c63349bfda1e95b63d2c4640c99ead4
unknown <mescher@gauss.inet.dkfz-heidelberg.de>
2016-06-03 11:55:02 +0200

    * improve pDVH calculation

--- matRad_calcPDVH.m
+++ matRad_calcPDVH.m
@@ -1,45 +1,31 @@
-function [dvhPoints,V] = matRad_calcPDVH(Coverage,doseVec,cst,numOfScenarios)
+function [dvhPoints,volume] = matRad_calcPDVH(coverage,doseVec,cst,numOfScenarios,dij)
 
-n         = 10000;
-numOfVois = size(cst,1);
+% set dose points in dvh
+dvhPoints = linspace(0,max(vertcat(doseVec{:}))*1.05,10000);
 
-% set DVH points
-dvhPoints = linspace(0,max(vertcat(doseVec{:}))*1.05,n);
-
-%figure
-for i = 1:numOfVois
-    dvh = repmat(NaN * ones(1,n),numOfScenarios,1);
+% calculate DVH in every scenario
+if length(doseVec) > 1
+    % use dij scenarios
     for Scen = 1:numOfScenarios
-        indices     = cst{i,4}{1};
-        numOfVoxels = numel(indices);
-        doseInVoi   = doseVec{Scen}(indices);
-
-        % calculate DVH
-        for j = 1:n
-            dvh(Scen,j) = sum(doseInVoi > dvhPoints(j));
-        end
-
-        dvh(Scen,:) = dvh(Scen,:) ./ numOfVoxels * 100;
+    doseInVoi   = doseVec{Scen}(cst{1,4}{1});
+    dvh(Scen,:) = sum(bsxfun(@ge,doseInVoi,dvhPoints))/numel(cst{1,4}{1})*100;    
     end
-
-    % calculate PDVH
-    for j = 1:n
-        VolumePointsSorted = sort(dvh(:,j),'descend');
-        ix = max([1 ceil(Coverage/100*numel(VolumePointsSorted))]);
-        V(i,j) = VolumePointsSorted(ix);
+elseif length(doseVec) == 1
+    % create scenarios with shifts
+    for Scen = 1:numOfScenarios
+    idxShift  = cst{1,5}.voxelShift(2,Scen) + cst{1,5}.voxelShift(1,Scen)*dij.dimensions(1) + cst{1,5}.voxelShift(3,Scen)*dij.dimensions(2)*dij.dimensions(1);
+    doseInVoi = doseVec{1}(cst{1,4}{1}-idxShift);
+    dvh(Scen,:) = sum(bsxfun(@ge,doseInVoi,dvhPoints))/numel(cst{1,4}{1})*100;
     end
-    
-    %plot(dvhPoints,V(i,:))
-    
-    % store legend info
-    %legendinfo{i} = [cst{i,2},' PDVH_{',num2str(Coverage(i)),'}'];
-
 end
 
-% set legend
-%legend(legendinfo)
-%grid on
-%xlabel('Dose [Gy]')
-%ylabel('Volume [%]')
+% calculate PDVH
+for j = 1:length(dvhPoints)
+    VolumePointsSorted = sort(dvh(:,j),'descend');
+    ix                 = max([1 ceil(coverage/100*numel(VolumePointsSorted))]);
+    volume(1,j)        = VolumePointsSorted(ix);
+end
+
+
 
 end
