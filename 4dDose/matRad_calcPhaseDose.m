718264e0ffecc6f9b928b91dfda3dd47e38518fd
Ulrich <sulrich@ad.dkfz-heidelberg.de>
2016-08-08 12:45:43 +0200

    calculate dose in each CT phase

--- 4dDose/matRad_calcPhaseDose.m
+++ 4dDose/matRad_calcPhaseDose.m
@@ -1,4 +1,4 @@
-function [resultGUI, delivery] = matRad_calcPhaseDose();
+function [resultGUI, delivery] = matRad_calcPhaseDose(delivery)
 % %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % 
 %
@@ -19,74 +19,71 @@ function [resultGUI, delivery] = matRad_calcPhaseDose();
 
 % get data from workspace
 multScen       = evalin('base','multScen');
+resultGUI      = evalin('base','resultGUI');
 dij            = evalin('base','dij');
-stf       = evalin('base','stf');
 
-NumberOfBeams = length(stf);
-for i=1:NumberOfBeams
-    allocate beam spots to time points
-    delivery(i).energy = zeros(length(delivery(i).es), 1); 
-    delivery(i).xpos = zeros(length(delivery(i).es), 1);
-    delivery(i).ypos = zeros(length(delivery(i).es), 1);
-    delivery(i).particles = zeros(length(delivery(i).es), 1);  % löschen???
+for i=1:length(delivery)
+    delivery(i).phase = zeros(length(delivery(i).es), 1); 
+    delivery(i).w = zeros(length(delivery(i).es), 1); 
+    delivery(i).index_j = zeros(length(delivery(i).es), 1); 
+    
+    %für richtige index der Bixel in Fluenzvektor
+    add = 0;
+    if(i>1)        
+        for k=2:i
+            add = length(delivery(k-1).w);       
+        end
+    end
+    
+    [j, index_j] = sort(delivery(i).j);
+    delivery(i).w = resultGUI.finalw(index_j + add);
+    delivery(i).index_j = index_j + add;
+
+  
+
+    %Motion assumption
+    NumOfPhases = multScen.numOfCtScen;
+    %Annahme 5s und linear  ÄNDERN
+    phaseTime = 5/NumOfPhases;
     j=1;
-    for IES = 1:delivery(i).energies
-        delivery(i).IES(IES).energy = str2num(xPln(i).PTTxPlanMd5.PTTxPlan.Beam.IES{1,IES}.Attributes.energy);
-         v=1;
-         jold = j;
-        %while(delivery(i).eop(j) == delivery(i).eop(jold))  % geht nicht da eop auch hochgezählt wird wenn Energie nicht geändert wird ???
-        while(v<delivery(i).IES(IES).voxel && j < length(delivery(i).es))  
-            while(j < length(delivery(i).es) && delivery(i).np(j)+1 == delivery(i).np(j+1))
-                delivery(i).energy(j) = str2num(xPln(i).PTTxPlanMd5.PTTxPlan.Beam.IES{1,IES}.Attributes.energy);
-                delivery(i).xpos(j) = str2num(xPln(i).PTTxPlanMd5.PTTxPlan.Beam.IES{1,IES}.Voxel{1,v}.Attributes.x);
-                delivery(i).ypos(j) = str2num(xPln(i).PTTxPlanMd5.PTTxPlan.Beam.IES{1,IES}.Voxel{1,v}.Attributes.y);
-                delivery(i).particles(j) = str2num(xPln(i).PTTxPlanMd5.PTTxPlan.Beam.IES{1,IES}.Voxel{1,v}.Attributes.particles);
-                j = j+1;
-                v=v+1;
-            end
-            while(j < length(delivery(i).es) && delivery(i).np(j) == delivery(i).np(j+1))
-                delivery(i).energy(j) = NaN;
-                delivery(i).xpos(j) =  NaN;
-                delivery(i).ypos(j) =  NaN;
-                delivery(i).particles(j) =  NaN;
-                j = j+1;
-            end
-        end               
-    end %IES
-
-    % find correct j-value for dose calculation with pre-calculated dij
-    % Matrix
-    % von Lucas export HITXMLPlan : HITXML, x-y-z(beam)
-    %                               matRad, x-y(beam)-z -> z-x-y(beam)
-    %      Koordinaten              voxel_x = rayPos_bev(3);
-    %                               voxel_y = rayPos_bev(1);
-         
-    delivery(i).stf_rayNum = zeros(length(delivery(i).es), 1); 
-    delivery(i).stf_energyIx = zeros(length(delivery(i).es), 1); 
-    delivery(i).j = zeros(length(delivery(i).es), 1); 
-    for c=1:length(delivery(i).time)-1
-        if(~isnan(delivery(i).xpos(c)))
-            delivery_pos = [delivery(i).xpos(c); delivery(i).ypos(c)]*ones(1,stf(i).numOfRays);
-        
-            stf_pos =reshape([stf(i).ray.rayPos_bev],3,[]);
-            rayNum = find(sum(stf_pos([3 1],:) == delivery_pos) == 2);
-        
-            energyIx = find(abs(stf(i).ray(rayNum).energy-delivery(i).energy(c))<0.1);
-
-            j = find(dij.rayNum == rayNum & dij.bixelNum == energyIx & dij.beamNum == i);
-            
-            delivery(i).stf_rayNum(c) = rayNum;
-            delivery(i).stf_energyIx(c) = energyIx;
-            delivery(i).j(c) = j;
-        else
-            delivery(i).stf_rayNum(c) = NaN;
-            delivery(i).stf_energyIx(c) = NaN;
-            delivery(i).j(c) = NaN;
+    p = 1;
+    Time = 0;
+    while(j < length(delivery(i).time))
+        Time = Time + phaseTime;
+        while(j < length(delivery(i).time) && delivery(i).time(j)/1000000 < Time)
+            delivery(i).phase(j) = p;
+            j= j+1;
+        end
+        p = p+1;
+        if(p > NumOfPhases)
+            p=1;
         end
     end    
-        
 end %beams
 
-NumOfPhases = multScen.numOfCtScen;
+%berechne Dosis für jede Phase  
+disp('calc dose in each CT phase')
+for p=1:NumOfPhases
+    w=zeros(dij.totalNumOfBixels, 1); 
+    for i=1:length(delivery)
+        t =  find(delivery(i).phase == p);
+        w(delivery(i).index_j(t)) = delivery(i).w(t);
+        Fluenzw{p} = w;
+    end
+ 
+    %Test    
+    %resultGUI.phaseDose{p} = reshape(dij.physicalDose{1,1} * w, dij.dimensions);
+    resultGUI.phaseDose{p} = reshape(dij.physicalDose{p,1} * w, dij.dimensions);
+   
+end
+
+%just for testing: dose summation
+resultGUI.sumDose = resultGUI.phaseDose{1}(:);
+for p=2:NumOfPhases
+    resultGUI.sumDose = resultGUI.sumDose + resultGUI.phaseDose{p}(:);
+end
+
+resultGUI.sumDose = reshape(resultGUI.sumDose, dij.dimensions);
+    
 
 
--- 4dDose/matRad_readLmdout.m
+++ 4dDose/matRad_readLmdout.m
@@ -1,4 +1,4 @@
-function delivery = matRad_readLmdout(FileName,NumberOfBeams)
+function delivery = matRad_readLmdout(FileName)
 % %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % reads in xml Plan file and lmdout file
 % one file per beam
@@ -25,7 +25,16 @@ function delivery = matRad_readLmdout(FileName,NumberOfBeams)
 % Silke Ulrich Aug 2016
 % %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
+disp('read in delivery information')
+
 delivery = struct;
+
+% get data from workspace
+%%% ok oder besser in Funktionsaufruf übergeben???
+dij            = evalin('base','dij');
+stf            = evalin('base','stf');
+
+NumberOfBeams = length(stf);
 
 for i=1:NumberOfBeams
     n = num2str(i-1);
@@ -72,7 +81,81 @@ for i=1:NumberOfBeams
         warningMessage = sprintf('Warning: Lmdoutfile does not exist');
         uiwait(msgbox(warningMessage));
     end
-      
+     
+    %allocate beam spots to time points
+    delivery(i).energy = zeros(length(delivery(i).es), 1); 
+    delivery(i).xpos = zeros(length(delivery(i).es), 1);
+    delivery(i).ypos = zeros(length(delivery(i).es), 1);
+    delivery(i).particles = zeros(length(delivery(i).es), 1);  % löschen???
+    j=1;
+    for IES = 1:delivery(i).energies
+        delivery(i).IES(IES).energy = str2num(xPln(i).PTTxPlanMd5.PTTxPlan.Beam.IES{1,IES}.Attributes.energy);
+         v=1;
+         jold = j;
+        %while(delivery(i).eop(j) == delivery(i).eop(jold))  % geht nicht da eop auch hochgezählt wird wenn Energie nicht geändert wird ???
+        while(v<=delivery(i).IES(IES).voxel && j < length(delivery(i).es))  
+            if(delivery(i).IES(IES).voxel == 1) %dann andere Struktur in xPln
+                delivery(i).energy(j) = str2num(xPln(i).PTTxPlanMd5.PTTxPlan.Beam.IES{1,IES}.Attributes.energy);
+                delivery(i).xpos(j) = str2num(xPln(i).PTTxPlanMd5.PTTxPlan.Beam.IES{1,IES}.Voxel.Attributes.x);
+                delivery(i).ypos(j) = str2num(xPln(i).PTTxPlanMd5.PTTxPlan.Beam.IES{1,IES}.Voxel.Attributes.y);
+                delivery(i).particles(j) = str2num(xPln(i).PTTxPlanMd5.PTTxPlan.Beam.IES{1,IES}.Voxel.Attributes.particles);
+                j = j+1;
+            else
+                while(j < length(delivery(i).es) && delivery(i).np(j)+1 == delivery(i).np(j+1))
+                    delivery(i).energy(j) = str2num(xPln(i).PTTxPlanMd5.PTTxPlan.Beam.IES{1,IES}.Attributes.energy);
+                    delivery(i).xpos(j) = str2num(xPln(i).PTTxPlanMd5.PTTxPlan.Beam.IES{1,IES}.Voxel{1,v}.Attributes.x);
+                    delivery(i).ypos(j) = str2num(xPln(i).PTTxPlanMd5.PTTxPlan.Beam.IES{1,IES}.Voxel{1,v}.Attributes.y);
+                    delivery(i).particles(j) = str2num(xPln(i).PTTxPlanMd5.PTTxPlan.Beam.IES{1,IES}.Voxel{1,v}.Attributes.particles);
+                    j = j+1;
+                    v=v+1;
+                end
+            end
+            while(j < length(delivery(i).es) && delivery(i).np(j) == delivery(i).np(j+1))
+                delivery(i).energy(j) = NaN;
+                delivery(i).xpos(j) =  NaN;
+                delivery(i).ypos(j) =  NaN;
+                delivery(i).particles(j) =  NaN;
+                j = j+1;
+            end
+        end  
+    end %IES
+    
+    delivery(i).energy(j) = NaN;
+    delivery(i).xpos(j) =  NaN;
+    delivery(i).ypos(j) =  NaN;
+    delivery(i).particles(j) =  NaN;
+
+    % find correct j-value for dose calculation with pre-calculated dij
+    % Matrix
+    % von Lucas export HITXMLPlan : HITXML, x-y-z(beam)
+    %                               matRad, x-y(beam)-z -> z-x-y(beam)
+    %      Koordinaten              voxel_x = rayPos_bev(3);
+    %                               voxel_y = rayPos_bev(1);
+         
+    delivery(i).stf_rayNum = zeros(length(delivery(i).es), 1); 
+    delivery(i).stf_energyIx = zeros(length(delivery(i).es), 1); 
+    delivery(i).j = zeros(length(delivery(i).es), 1); 
+    for c=1:length(delivery(i).time)
+        if(~isnan(delivery(i).xpos(c)))
+            delivery_pos = [delivery(i).xpos(c); delivery(i).ypos(c)]*ones(1,stf(i).numOfRays);
+        
+            stf_pos =reshape([stf(i).ray.rayPos_bev],3,[]);
+            rayNum = find(sum(stf_pos([3 1],:) == delivery_pos) == 2);
+        
+            energyIx = find(abs(stf(i).ray(rayNum).energy-delivery(i).energy(c))<0.1);
+
+            j = find(dij.rayNum == rayNum & dij.bixelNum == energyIx & dij.beamNum == i);
+            
+            delivery(i).stf_rayNum(c) = rayNum;
+            delivery(i).stf_energyIx(c) = energyIx;
+            delivery(i).j(c) = j;
+        else
+            delivery(i).stf_rayNum(c) = NaN;
+            delivery(i).stf_energyIx(c) = NaN;
+            delivery(i).j(c) = NaN;
+        end
+    end    
+    
 end %beams
 
 
