9281b33a6ee34f162fa24e9a128aa7b02c437fca
Niklas Wahl <n.wahl@dkfz.de>
2016-10-05 15:04:31 +0200

    added Copyright notice

--- IO/matRad_importGUI.m
+++ IO/matRad_importGUI.m
@@ -24,6 +24,19 @@ function varargout = matRad_importGUI(varargin)
 
 % Last Modified by GUIDE v2.5 04-Oct-2016 14:21:41
 
+% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%
+% Copyright 2015 the matRad development team. 
+% 
+% This file is part of the matRad project. It is subject to the license 
+% terms in the LICENSE file found in the top-level directory of this 
+% distribution and at https://github.com/e0404/matRad/LICENSES.txt. No part 
+% of the matRad project, including this file, may be copied, modified, 
+% propagated, or distributed except according to the terms contained in the 
+% LICENSE file.
+%
+% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
 % Begin initialization code - DO NOT EDIT
 gui_Singleton = 1;
 gui_State = struct('gui_Name',       mfilename, ...
