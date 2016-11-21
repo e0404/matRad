551915d4e266b5c88ee919aa09d2a54d135363f3
AD\mescher <h.mescher@dkfz-heidelberg.de>
2016-11-21 12:01:41 +0100

    * include pointwise probability calc for multiple Dij scenarios (shift scenarios)

--- matRad_calcScenProb.m
+++ matRad_calcScenProb.m
@@ -1,9 +1,9 @@
 function scenProb = matRad_calcScenProb(mu,sigma,samplePos,calcType,probDist)
 
 if isequal(probDist,'normDist')
+    scenProb = 1;
+    
     if isequal(calcType,'probBins')
-        
-        scenProb = 1;
         
         for i = 1:length(mu)
             samplePosSorted = sort(unique(samplePos(i,:)));
@@ -14,13 +14,15 @@ if isequal(probDist,'normDist')
             scenProb        = scenProb.*0.5.*(erf((upperBinLevel-mu(i))/(sqrt(2)*sigma(i)))-erf((lowerBinLevel-mu(i))/(sqrt(2)*sigma(i))));
         end
         
-        % normalize probabilities since we use only a subset of
-        % the 3D grid 
-        scenProb = scenProb./sum(scenProb);
-        
-    else
-        error('Until now, only probBins implemented')
+    elseif isequal(calcType,'pointwise')
+        for i = 1:length(mu)
+            scenProb = scenProb .* (1/sqrt(2*pi*sigma(i)^2)*exp(-((samplePos(i,:)-mu(i)).^2./(2*sigma(i)^2))));
+        end
     end
+    
+    % normalize probabilities since we use only a subset of
+    % the 3D grid 
+    scenProb = scenProb./sum(scenProb);
     
 else
     error('Until now, only normally distributed scenarios implemented')
