1dbbca622130838cb9213672d0f33032fd14beb8
Lucas-Rapahel Müller <gitdev@LRM.PHOTO>
2017-02-01 19:12:25 +0100

    Add help and license

--- dicomImport/matRad_loadHLUT.m
+++ dicomImport/matRad_loadHLUT.m
@@ -1,5 +1,33 @@
 function hlut = matRad_loadHLUT(ct)
-% load hlut
+  % %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+  % matRad function to load HLUT file based on the provided ct
+  %
+  % call
+  %   hlut = matRad_loadHLUT(ct)
+  %
+  % input
+  %   ct: unprocessed dicom ct data 
+  %
+  % output
+  %   hlut:   lookup table
+  %
+  % References
+  %   -
+  %
+  % %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+  % %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+  %
+  % Copyright 2015 the matRad development team. 
+  % 
+  % This file is part of the matRad project. It is subject to the license 
+  % terms in the LICENSE file found in the top-level directory of this 
+  % distribution and at https://github.com/e0404/matRad/LICENSES.txt. No part 
+  % of the matRad project, including this file, may be copied, modified, 
+  % propagated, or distributed except according to the terms contained in the 
+  % LICENSE file.
+  %
+  % %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 % directory with look up table files
 hlutDir = fullfile(fileparts(mfilename('fullpath')),'hlutLibrary',filesep);
@@ -50,4 +78,3 @@ hlutFile = fopen(hlutFileName,'r');
 hlut = cell2mat(textscan(hlutFile,'%f %f','CollectOutput',1,'commentStyle','#'));
 fclose(hlutFile);
 end
-
