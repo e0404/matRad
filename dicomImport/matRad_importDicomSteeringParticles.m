ff8b5ae7bc1653bb6a89debac0a4fa6ad084a3a3
Lucas-Rapahel Müller <gitdev@LRM.PHOTO>
2017-03-14 10:40:59 +0100

    Change order

--- dicomImport/matRad_importDicomSteeringParticles.m
+++ dicomImport/matRad_importDicomSteeringParticles.m
@@ -38,9 +38,9 @@ function [stf, pln] = matRad_importDicomSteeringParticles(ct, pln, rtPlanFile)
 %% load plan file
 % load machine data
 
-% messagebox only necessary for non windows users
 dlgBaseDataText = ['Import steering information from DICOM Plan.','Choose corresponding matRad base data for ', ...
         pln.radiationMode, '.'];
+% messagebox only necessary for non windows users
 if ~ispc
     uiwait(helpdlg(dlgBaseDataText,['DICOM import - ', pln.radiationMode, ' base data' ]));
 end
