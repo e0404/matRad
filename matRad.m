867dc6db61a840e911ef87ee2c6fbd4e30ec6180
Hans-Peter Wieser <h.wieser@dkfz-heidelberg.de>
2017-10-13 17:19:27 +0200

    revert changes to matRad.m

--- matRad.m
+++ matRad.m
@@ -29,19 +29,19 @@ load TG119.mat
 
 % meta information for treatment plan
 pln.bixelWidth      = 5; % [mm] / also corresponds to lateral spot spacing for particles
-pln.gantryAngles    = [0 90]; % [°]
-pln.couchAngles     = [0 -10]; % [°]
+pln.gantryAngles    = [0:72:359]; % [?]
+pln.couchAngles     = [0 0 0 0 0]; % [?]
 pln.numOfBeams      = numel(pln.gantryAngles);
 pln.numOfVoxels     = prod(ct.cubeDim);
 pln.isoCenter       = ones(pln.numOfBeams,1) * matRad_getIsoCenter(cst,ct,0);
 pln.voxelDimensions = ct.cubeDim;
-pln.radiationMode   = 'protons';     % either photons / protons / carbon
+pln.radiationMode   = 'photons';     % either photons / protons / carbon
 pln.bioOptimization = 'none';        % none: physical optimization;             const_RBExD; constant RBE of 1.1;
                                      % LEMIV_effect: effect-based optimization; LEMIV_RBExD: optimization of RBE-weighted dose
 pln.numOfFractions  = 30;
 pln.runSequencing   = false; % 1/true: run sequencing, 0/false: don't / will be ignored for particles and also triggered by runDAO below
 pln.runDAO          = false; % 1/true: run DAO, 0/false: don't / will be ignored for particles
-pln.machine         = 'HITfixedBL';
+pln.machine         = 'Generic';
 
 %% initial visualization and change objective function settings if desired
 matRadGUI
