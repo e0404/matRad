043a7e02768a989dcdadd5cfc0056dfb7e3094c2
Johanna Winter <johanna.winter@dkfz.de>
2017-11-16 10:09:50 +0100

    created wrapper function for computing VOI contours

--- matRadGUI.m
+++ matRadGUI.m
@@ -354,17 +354,7 @@ try
         cst = evalin('base','cst');
         cst = setCstTable(handles,cst);
         handles.State = 1;
-        % check if contours are precomputed
-        if size(cst,2) < 7
-            cst = matRad_computeVoiContours(ct,cst);
-        else
-            for i = 1:size(cst,1)
-                if isempty(cst{i,7})
-                    cst = matRad_computeVoiContours(ct,cst);
-                    break
-                end
-            end
-        end
+        cst = matRad_computeVoiContoursWrapper(cst,ct);
         assignin('base','cst',cst);
 
     elseif ismember('ct',AllVarNames) &&  ~ismember('cst',AllVarNames)
@@ -458,17 +448,7 @@ try
     cst = setCstTable(handles,cst);
     handles.TableChanged = false;
     set(handles.popupTypeOfPlot,'Value',1);
-    % precompute contours if necessary
-    if size(cst,2) < 7
-        cst = matRad_computeVoiContours(ct,cst);
-    else
-        for i = 1:size(cst,1)
-            if isempty(cst{i,7})
-                cst = matRad_computeVoiContours(ct,cst);
-                break
-            end
-        end
-    end
+    cst = matRad_computeVoiContoursWrapper(cst,ct);
 
     assignin('base','ct',ct);
     assignin('base','cst',cst);
@@ -2786,17 +2766,7 @@ try
         cst = evalin('base','cst');
         cst = setCstTable(handles,cst);
         handles.State = 1;
-        % check if contours are precomputed
-        if size(cst,2) < 7
-            cst = matRad_computeVoiContours(ct,cst);
-        else
-            for i = 1:size(cst,1)
-                if isempty(cst{i,7})
-                    cst = matRad_computeVoiContours(ct,cst);
-                    break
-                end
-            end
-        end
+        cst = matRad_computeVoiContoursWrapper(cst,ct);
         assignin('base','cst',cst);
     elseif ismember('ct',AllVarNames) &&  ~ismember('cst',AllVarNames)
          handles = showError(handles,'GUI OpeningFunc: could not find cst file');
