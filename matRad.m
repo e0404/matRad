f090c6416da3a1bd5bf5578469afe7b081936bba
turing\localbangertm <markbangert@gmail.com>
2018-02-14 14:23:58 +0100

    rename

--- matRad.m
+++ matRad.m
@@ -35,18 +35,18 @@ pln.machine         = 'Generic';
 pln.numOfFractions  = 30;
 
 % beam geometry settings
-pln.propertiesStf.bixelWidth      = 5; % [mm] / also corresponds to lateral spot spacing for particles
-pln.propertiesStf.gantryAngles    = [0:72:359]; % [?]
-pln.propertiesStf.couchAngles     = [0 0 0 0 0]; % [?]
-pln.propertiesStf.numOfBeams      = numel(pln.propertiesStf.gantryAngles);
-pln.propertiesStf.isoCenter       = ones(pln.propertiesStf.numOfBeams,1) * matRad_getIsoCenter(cst,ct,0);
+pln.propStf.bixelWidth      = 5; % [mm] / also corresponds to lateral spot spacing for particles
+pln.propStf.gantryAngles    = [0:72:359]; % [?]
+pln.propStf.couchAngles     = [0 0 0 0 0]; % [?]
+pln.propStf.numOfBeams      = numel(pln.propertiesStf.gantryAngles);
+pln.propStf.isoCenter       = ones(pln.propertiesStf.numOfBeams,1) * matRad_getIsoCenter(cst,ct,0);
 
 
 % optimization settings
-pln.propertiesOpt.bioOptimization = 'none'; % none: physical optimization;             const_RBExD; constant RBE of 1.1;
+pln.propOpt.bioOptimization = 'none'; % none: physical optimization;             const_RBExD; constant RBE of 1.1;
                                             % LEMIV_effect: effect-based optimization; LEMIV_RBExD: optimization of RBE-weighted dose
-pln.propertiesOpt.runDAO          = false;  % 1/true: run DAO, 0/false: don't / will be ignored for particles
-pln.propertiesOpt.runSequencing   = false;  % 1/true: run sequencing, 0/false: don't / will be ignored for particles and also triggered by runDAO below
+pln.propOpt.runDAO          = false;  % 1/true: run DAO, 0/false: don't / will be ignored for particles
+pln.propOpt.runSequencing   = false;  % 1/true: run sequencing, 0/false: don't / will be ignored for particles and also triggered by runDAO below
 
 %% initial visualization and change objective function settings if desired
 matRadGUI
