9301c6db066df13b0363325743ef1de384e2ba7b
ahmadnish <neishabouri@stud.uni-heidelberg.de>
2018-08-03 18:30:04 +0200

    changing the prepared dvf from voxel units to mm; adding plotting; return if there is already a dvf

--- 4D/matRad_addMovement.m
+++ 4D/matRad_addMovement.m
@@ -1,10 +1,14 @@
-function ct = matRad_addMovement(ct, motionPeriod, amp)
+function ct = matRad_addMovement(ct, motionPeriod, numOfCtScen, amp)
 
-if nargin < 3
-    amp = [5 0 0];
+if nargin < 4
+    amp = [0 5 0];
 end
 
-for i = 1:ct.numOfCtScen
+% if isfield(ct,'dvf')
+%     return
+% end
+figure('units','normalized','outerposition',[0 0 1 1])
+for i = 1:numOfCtScen
     
     im = ct.cube{1};
     
@@ -14,8 +18,25 @@ for i = 1:ct.numOfCtScen
     ct.dvf{i}(:,:,:,2) = amp(2) * sin(i * pi / (motionPeriod * 2));
     ct.dvf{i}(:,:,:,3) = amp(3) * sin(i * pi / (motionPeriod * 2));
     
-    if i~=ct.numOfCtScen
+    if i~=numOfCtScen 
         ct.cube{i+1} = imwarp(im, ct.dvf{i});
+        
+        % plotting
+        subplot(121)
+        imagesc(im(:,:,120))
+        subplot(122)
+        imagesc(ct.cube{i+1}(:,:,120))
+        title(i)
+        pause(.2)
+        hold on
     end
     
+    ct.dvf{i}(:,:,:,1) = ct.dvf{i}(:,:,:,1) * ct.resolution.x;
+    ct.dvf{i}(:,:,:,2) = ct.dvf{i}(:,:,:,2) * ct.resolution.y;
+    ct.dvf{i}(:,:,:,3) = ct.dvf{i}(:,:,:,3) * ct.resolution.z;
+    ct.dvf{i} = permute(ct.dvf{i}, [4,1,2,3]);
+    
 end
+close
+end
+
