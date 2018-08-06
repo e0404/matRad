747e37843fdaafbb0e0ae13e9df692854fba88b2
ahmadnish <neishabouri@stud.uni-heidelberg.de>
2018-08-06 12:15:48 +0200

    code improvement: making first dvf zero as it is in the liver case

--- 4D/matRad_addMovement.m
+++ 4D/matRad_addMovement.m
@@ -4,32 +4,35 @@ if nargin < 4
     amp = [0 5 0];
 end
 
-% if isfield(ct,'dvf')
-%     return
-% end
-figure('units','normalized','outerposition',[0 0 1 1])
+% if there is already a dvf, no need to addMovement
+if isfield(ct,'dvf') 
+    return
+end
+
+figure %temp
+
 for i = 1:numOfCtScen
     
     im = ct.cube{1};
     
     ct.dvf{i} = zeros([size(im), 3]);
     
-    ct.dvf{i}(:,:,:,1) = amp(1) * sin(i * pi / (motionPeriod * 2));
-    ct.dvf{i}(:,:,:,2) = amp(2) * sin(i * pi / (motionPeriod * 2));
-    ct.dvf{i}(:,:,:,3) = amp(3) * sin(i * pi / (motionPeriod * 2));
+    ct.dvf{i}(:,:,:,1) = amp(1) * sin((i - 1) * pi / (motionPeriod * 2));
+    ct.dvf{i}(:,:,:,2) = amp(2) * sin((i - 1) * pi / (motionPeriod * 2));
+    ct.dvf{i}(:,:,:,3) = amp(3) * sin((i - 1) * pi / (motionPeriod * 2));
     
-    if i~=numOfCtScen 
-        ct.cube{i+1} = imwarp(im, ct.dvf{i});
-        
-        % plotting
-        subplot(121)
-        imagesc(im(:,:,120))
-        subplot(122)
-        imagesc(ct.cube{i+1}(:,:,120))
-        title(i)
-        pause(.2)
-        hold on
-    end
+
+    ct.cube{i} = imwarp(im, ct.dvf{i});
+    
+    % plotting %temp{
+    subplot(121)
+    imagesc(im(:,:,120))
+    subplot(122)
+    imagesc(ct.cube{i}(:,:,120))
+    title(i)
+    pause(.8)
+    hold on  %}
+
     
     ct.dvf{i}(:,:,:,1) = ct.dvf{i}(:,:,:,1) * ct.resolution.x;
     ct.dvf{i}(:,:,:,2) = ct.dvf{i}(:,:,:,2) * ct.resolution.y;
@@ -37,6 +40,6 @@ for i = 1:numOfCtScen
     ct.dvf{i} = permute(ct.dvf{i}, [4,1,2,3]);
     
 end
-close
+close % temp
 end
 
