5f3e23a07876dbded635d2c4b0ea602f7d7c4145
ahmadnish <neishabouri@stud.uni-heidelberg.de>
2018-08-23 19:39:30 +0200

    travis pipeline pruning

--- unitTest/runMatRadTests.m
+++ unitTest/runMatRadTests.m
@@ -1,32 +1,11 @@
 function statusAll = runMatRadTests(varargin)
 %% This file runs the complete matRad test suite.
 
-
-CI_MODE = strcmpi(getenv('CONTINUOUS_INTEGRATION'),'true') || strcmp(getenv('CI'),'true');
-isJenkins = ~isempty(getenv('JENKINS_URL'));
-
 %% Set path
-addpath(fullfile(pwd,'suites'));
+
 addpath(fullfile(pwd,'..'));
 addpath(fullfile(pwd,'..','examples'));
 
-%% Select functions to runs
-suite = @coreTest;
-allTests = 1:numel(suite(0));
-
-%% Prepare environment
-if strcmpi(getEnvironment(), 'Octave')
-  % Ensure that paging is disabled
-  % https://www.gnu.org/software/octave/doc/interpreter/Paging-Screen-Output.html
-  more off
-end
-
-%% Run tests
-status = coreTest(1);
-
 matRad_example5_protons
 
-%% Calculate exit code
-if CI_MODE
-    exit(nErrors);
-end
+exit
