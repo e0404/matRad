2529a9950d592aca579fc07e1fb7a7c613b1d349
ahmadnish <neishabouri@stud.uni-heidelberg.de>
2018-08-28 15:58:48 +0200

    needs VMC++ files

--- unitTest/runMatRadTests.m
+++ unitTest/runMatRadTests.m
@@ -14,8 +14,6 @@ matRad_example2_photons
 disp('example 2 done')
 matRad_example3_photonsDAO
 disp('example 3 done')
-matRad_example4_photonsVMC
-disp('example 4 done')
 matRad_example5_protons
 disp('example 5 done')
 matRad_example6_protonsNoise
