2fe470c36be0f6c3b2d824410e02354fc72815cd
ahmadnish <neishabouri@stud.uni-heidelberg.de>
2018-08-28 15:03:15 +0200

    adding example3_photonsDAO as another unit test

--- unitTest/runMatRadTests.m
+++ unitTest/runMatRadTests.m
@@ -8,7 +8,12 @@ addpath(fullfile(pwd,'..','dicomImport'));
 addpath(fullfile(pwd,'..','tools'));
 
 matRad_example5_protons
+disp('example 5 done')
 matRad_example1_phantom
+disp('example 1 done')
 matRad_example2_photons
+disp('example 2 done')
+matRad_example3_photonsDAO
+disp('example 3 done')
 
 exit
