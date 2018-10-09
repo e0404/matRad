e52b450f7853a53bb0f0633a1024160608d324c4
ahmadnish <neishabouri@stud.uni-heidelberg.de>
2018-10-09 17:15:49 +0200

    preparing for the pull request; adding flags for supressing the output

--- unitTest/runMatRadTests.m
+++ unitTest/runMatRadTests.m
@@ -2,7 +2,7 @@
 
 %% Set path
 clc; clear; close all
-addpath(fullfile(pwd,'..'));
+addpath(genpath(fullfile(pwd,'..')));
 
 
 % limiting the optimization to 10 iterations
@@ -16,17 +16,12 @@ cd ./../unitTest/
 % supressing the inherent Ocatave warnings for division by zero
 warning("off", "Octave:divide-by-zero")
 
-unitTests = {@matRad_example0, ...
-             @matRad_example1_phantom, ...
-             @matRad_example2_photons, ...
-             @matRad_example3_photonsDAO, ...
-             @matRad_example5_protons, ...
-             @matRad_example6_protonsNoise, ...
-             @matRad_example7_carbon, ...
-             @matRad_example8_protonsRobust};
+param.logLevel = 3;
 
-for k = 1:length(unitTests);
-    
-    [cst, stf, pln, ct, dij, resultGUI] = unitTests{k}();
+matRad_example1_phantom
+matRad_example2_photons
+matRad_example3_photonsDAO
+% matRad_example5_protons
+% matRad_example6_protonsNoise
+% matRad_example7_carbon
 
-end
