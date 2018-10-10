d69f774863794a3b2da0ec005ce863a3facd8aa2
ahmadnish <neishabouri@stud.uni-heidelberg.de>
2018-10-10 11:18:37 +0200

    Revert "preparing for the pull request; fixing the issues with PROSTATE.mat and protons_Generic.mat, matRad_multScen"
    This reverts commit d18719d86b07190f10305aaf948256adfad0eda4.

--- unitTest/runMatRadTests.m
+++ unitTest/runMatRadTests.m
@@ -21,7 +21,7 @@ param.logLevel = 3;
 % matRad_example1_phantom
 % matRad_example2_photons
 % matRad_example3_photonsDAO
-% matRad_example5_protons
+matRad_example5_protons
 matRad_example6_protonsNoise
 matRad_example7_carbon
 matRad_example8_protonsRobust
