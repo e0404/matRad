9e15c056bbb3c77be04bf627d26017e65559f037
TURING\localbangertm <markbangert@gmail.com>
2018-10-23 22:55:47 +0200

    bugfix in 3d viewer

--- plotting/matRad_plotCtSlice3D.m
+++ plotting/matRad_plotCtSlice3D.m
@@ -45,7 +45,7 @@ if nargin < 6 || isempty(cMap)
 end
 
 if nargin < 7 || isempty(window)
-    window = [min(ct.cube{cubeIdx}(:)) max(ct.cube{cubeIdx}(:))];    
+    window = [min(ct.cubeHU{cubeIdx}(:)) max(ct.cubeHU{cubeIdx}(:))];    
 end
 
 cMapScale = size(cMap,1) - 1;
@@ -62,17 +62,17 @@ coords{3} = ct.resolution.z * (1:ct.cubeDim(3));
 if plane == 1 % Coronal plane
     [xMesh,zMesh] = meshgrid(coords{1},coords{3});
     yMesh = ctSlice*ct.resolution.x*ones(ct.cubeDim(3),ct.cubeDim(1));
-    ct_rgb = ind2rgb(uint8(cMapScale*(squeeze((ct.cube{cubeIdx}(ctSlice,:,:)-window(1))/(window(2) - window(1))))),cMap);
+    ct_rgb = ind2rgb(uint8(cMapScale*(squeeze((ct.cubeHU{cubeIdx}(ctSlice,:,:)-window(1))/(window(2) - window(1))))),cMap);
     ct_rgb = permute(ct_rgb,[2 1 3]);
 elseif plane == 2 % sagittal plane
     [yMesh,zMesh] = meshgrid(coords{2},coords{3});
     xMesh = ctSlice*ct.resolution.y*ones(ct.cubeDim(3),ct.cubeDim(2));
-    ct_rgb = ind2rgb(uint8(cMapScale*(squeeze((ct.cube{cubeIdx}(:,ctSlice,:)-window(1))/(window(2) - window(1))))),cMap);
+    ct_rgb = ind2rgb(uint8(cMapScale*(squeeze((ct.cubeHU{cubeIdx}(:,ctSlice,:)-window(1))/(window(2) - window(1))))),cMap);
     ct_rgb = permute(ct_rgb,[2 1 3]);
 elseif plane == 3 % Axial plane
     [xMesh,yMesh] = meshgrid(coords{1},coords{2});
     zMesh = ctSlice*ct.resolution.z*ones(ct.cubeDim(2),ct.cubeDim(1));    
-    ct_rgb = ind2rgb(uint8(cMapScale*(squeeze((ct.cube{cubeIdx}(:,:,ctSlice)-window(1))/(window(2) - window(1))))),cMap);
+    ct_rgb = ind2rgb(uint8(cMapScale*(squeeze((ct.cubeHU{cubeIdx}(:,:,ctSlice)-window(1))/(window(2) - window(1))))),cMap);
 end
 ctHandle = surface('XData',xMesh, 'YData',yMesh, 'ZData',zMesh, ...
         'CData',ct_rgb, 'CDataMapping','direct', ...
@@ -88,9 +88,9 @@ slicePlane{3} = [];
 
 slicePlane{plane} = coords{plane}(ctSlice);
 
-ct_rgb = ind2rgb(uint8(cMapScale*((ct.cube{cubeIdx}-window(1))/(window(2) - window(1)))),cMap);
+ct_rgb = ind2rgb(uint8(cMapScale*((ct.cubeHU{cubeIdx}-window(1))/(window(2) - window(1)))),cMap);
 
-ctHandle = slice(axesHandle,xMesh,yMesh,zMesh,ct.cube{cubeIdx},slicePlane{:});
+ctHandle = slice(axesHandle,xMesh,yMesh,zMesh,ct.cubeHU{cubeIdx},slicePlane{:});
 
 set(ctHandle,'EdgeColor','none');
 %}
