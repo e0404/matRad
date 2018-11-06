bc8ba1b6c49dcfef93433555a05ddd4dae3d98f2
ahmadnish <neishabouri@stud.uni-heidelberg.de>
2018-11-06 20:00:53 +0100

    making matRad_unitTestTextManipulation accepting filename, string to be replaced, and string to be inserted as arguments

--- unitTest/matRad_unitTestTextManipulation.m
+++ unitTest/matRad_unitTestTextManipulation.m
@@ -1,13 +1,13 @@
-function matRad_unitTestTextManipulation()
+function matRad_unitTestTextManipulation(filename, string1, string2)
 
-fid=fopen('matRad_calcPhotonDose.m');
-fo=fopen('tempFile1.m','w');
+fid=fopen(filename);
+fo=fopen('tempFile.m','w');
 tline = fgetl(fid);
 
 while ischar(tline)
     
-    if strfind(tline, 'lateralCutoff = 50')
-        fprintf(fo, '%s\n', 'lateralCutoff = 20;');
+    if strfind(tline, string1)
+        fprintf(fo, '%s\n', string2);
     else
         fprintf(fo, '%s\n', tline);
     end
@@ -17,44 +17,6 @@ end
 fclose(fid);
 fclose(fo);
 
-movefile('tempFile1.m', '../matRad_calcPhotonDose.m', 'f');
-
-
-fid=fopen('matRad_calcParticleDose.m');
-fo=fopen('tempFile2.m','w');
-tline = fgetl(fid);
-while ischar(tline)
-    
-    if strfind(tline, 'cutOffLevel          = 0.99')
-        fprintf(fo, '%s\n', '       cutOffLevel          = 0.8;');
-    else
-        fprintf(fo, '%s\n', tline);
-    end
-    tline = fgetl(fid);
-end
-
-fclose(fid);
-fclose(fo);
-
-movefile('tempFile2.m', '../matRad_calcParticleDose.m', 'f');
-
-fid=fopen('matRad_ipoptOptions.m');
-fo=fopen('tempFile3.m','w');
-tline = fgetl(fid);
-
-while ischar(tline)
-    
-    if strfind(tline, 'options.ipopt.max_iter')
-        fprintf(fo, '%s\n', 'options.ipopt.max_iter = 10;');
-    else
-        fprintf(fo, '%s\n', tline);
-    end
-    tline = fgetl(fid);
-end
-
-fclose(fid);
-fclose(fo);
-
-movefile('tempFile3.m', '../optimization/matRad_ipoptOptions.m', 'f');
+movefile('tempFile.m', '../matRad_calcPhotonDose.m', 'f');
 end
 
--- unitTest/runMatRadTests.m
+++ unitTest/runMatRadTests.m
@@ -7,7 +7,10 @@ addpath(genpath(fullfile(pwd,'..')));
 
 % limiting the optimization to 10 iterations for faster computation
 % limiting the cutoffLevel and lateralCutoff for faster computation
-matRad_unitTestTextManipulation()
+matRad_unitTestTextManipulation('matRad_calcPhotonDose.m', 'lateralCutoff = 50', 'lateralCutoff = 20;')
+matRad_unitTestTextManipulation('matRad_calcParticleDose.m', 'cutOffLevel          = 0.99', '       cutOffLevel          = 0.8;')
+matRad_unitTestTextManipulation('matRad_ipoptOptions.m', 'options.ipopt.max_iter', 'options.ipopt.max_iter = 10;')
+
 
 % supressing the inherent Ocatave warnings for division by zero
 warning("off", "Octave:divide-by-zero")
