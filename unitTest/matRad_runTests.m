dcac82b643a65352e7efea5502651ae8c9b15dc7
TURING\localbangertm <markbangert@gmail.com>
2018-11-28 23:03:58 +0100

    update example and runTests

--- unitTest/matRad_runTests.m
+++ unitTest/matRad_runTests.m
@@ -17,11 +17,19 @@ end
 
 unitTestBool = true;
 
+disp('Unit test run example 1');
 matRad_example1_phantom
+disp('Unit test run example 2');
 matRad_example2_photons
+disp('Unit test run example 3');
 matRad_example3_photonsDAO
+disp('Unit test run example 5');
 matRad_example5_protons
+disp('Unit test run example 6');
 matRad_example6_protonsNoise
+disp('Unit test run example 7');
 matRad_example7_carbon
+disp('Unit test run example 8');
 matRad_example8_protonsRobust
+disp('Unit test run matRad script');
 matRad
