0cf6eacb70288dec13746b0dfcd5096b3cfb1ce0
Niklas Wahl <niklaswahl2011@gmail.com>
2018-11-29 21:45:19 -0300

    delete old files and reorganize mex files

--- optimization/matRad_createProjection.m
+++
@@ -1,7 +0,0 @@
-function [outputArg1,outputArg2] = matRad_createProjection(inputArg1,inputArg2)
-%MATRAD_CREATEPROJECTION Summary of this function goes here
-%   Detailed explanation goes here
-outputArg1 = inputArg1;
-outputArg2 = inputArg2;
-end
-
