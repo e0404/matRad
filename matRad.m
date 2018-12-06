9d821351e7044532711f9728cdc52c40cfceb4f8
Niklas Wahl <niklaswahl2011@gmail.com>
2018-12-06 10:58:03 +0100

    fix propertiesOpt to propOpt

--- matRad.m
+++ matRad.m
@@ -66,7 +66,7 @@ end
 resultGUI = matRad_fluenceOptimization(dij,cst,pln);
 
 %% sequencing
-if strcmp(pln.radiationMode,'photons') && (pln.propertiesOpt.runSequencing || pln.propertiesOpt.runDAO)
+if strcmp(pln.radiationMode,'photons') && (pln.propOpt.runSequencing || pln.propOpt.runDAO)
     %resultGUI = matRad_xiaLeafSequencing(resultGUI,stf,dij,5);
     %resultGUI = matRad_engelLeafSequencing(resultGUI,stf,dij,5);
     resultGUI = matRad_siochiLeafSequencing(resultGUI,stf,dij,5);
