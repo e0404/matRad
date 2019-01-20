246908b6216dda2b3952e0f0731459aec48cc08c
TURING\localbangertm <markbangert@gmail.com>
2019-01-20 22:05:36 +0100

    modify sparse matrix mex reader to perform flipping

--- matRad_sparseBeamletsReaderMSsquare.c
+++ matRad_sparseBeamletsReaderMSsquare.c
@@ -1,4 +1,23 @@
 /*
+% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%
+% Copyright 2019 the matRad development team. 
+% 
+% This file is part of the matRad project. It is subject to the license 
+% terms in the LICENSE file found in the top-level directory of this 
+% distribution and at https://github.com/e0404/matRad/LICENSES.txt. No part 
+% of the matRad project, including this file, may be copied, modified, 
+% propagated, or distributed except according to the terms contained in the 
+% LICENSE file.
+%
+% The original version of this file is part of openREGGUI and can be found 
+% at https://openreggui.org/git/open/REGGUI/blob/master/plugins/openMIROpt/
+% functions/io/mexSparseBeamletsReader_double_OptROIvoxels.c
+%
+% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+*/
+
+/*
 compile with matlab: mex -largeArrayDims mexSparseBeamletsReader_double_OptROIvoxels.c
 run with matlab: Beamlets = mexSparseBeamletsReader('Sparse_Dose.bin', [256 256 71], 8372);
 */
@@ -29,6 +48,8 @@ void mexFunction(   int nlhs,   mxArray *plhs[],    int nrhs,   const mxArray *p
     DoseGridSize[1] = *(mxGetPr(prhs[1])+1);
     DoseGridSize[2] = *(mxGetPr(prhs[1])+2);
     int NbrVoxels = (int)DoseGridSize[0]*DoseGridSize[1]*DoseGridSize[2];
+    int NbrVoxelsSlice = (int)DoseGridSize[0]*DoseGridSize[1];
+
 
     /* third input must be numeric */
     if(mxIsNumeric(prhs[2]) != 1) mexErrMsgIdAndTxt( "mexSparseReader:InvalidNbrSpots", "Third argument (number of spots) must be numeric.");
@@ -67,11 +88,14 @@ void mexFunction(   int nlhs,   mxArray *plhs[],    int nrhs,   const mxArray *p
     int i, j;
     float *data = mxCalloc(NbrVoxels, sizeof(float));
     
+    int ixMatRad, ixMC2, iSub, jSub, kSub;
+    
     for(i=0; i<NbrSpots; i++){
         
         /* mexPrintf("Spot %d / %d \n", i+1, NbrSpots); */
         
         NbrBytesRead = fread(&NonZeroVoxels, sizeof(uint32_t), 1, fid);
+        //mexPrintf("pencil beam %d -> %d nonzero voxels \n", i, NonZeroVoxels);
         NbrBytesRead = fread(&BeamID, sizeof(uint32_t), 1, fid);
         NbrBytesRead = fread(&LayerID, sizeof(uint32_t), 1, fid);
         NbrBytesRead = fread(&xcoord, sizeof(float), 1, fid);
@@ -79,7 +103,8 @@ void mexFunction(   int nlhs,   mxArray *plhs[],    int nrhs,   const mxArray *p
     
         ReadVoxels = 0;
         Sparse_JC[i] = NbrElements;
-  
+        //mexPrintf("ix = %d -> value = %d \n",i , NbrElements);
+        
         while(1){
             NbrBytesRead = fread(&NbrContinuousValues, sizeof(uint32_t), 1, fid);
             ReadVoxels += NbrContinuousValues;
@@ -111,7 +136,25 @@ void mexFunction(   int nlhs,   mxArray *plhs[],    int nrhs,   const mxArray *p
                 
                 if (x[FirstIndex-j-1]){
                     Sparse_data[NbrElements] = (double)data[j];
-                    Sparse_index[NbrElements] = FirstIndex-j-1;
+                    
+                    // get index
+                    ixMC2 = NbrVoxels - (FirstIndex-j-1);
+                    
+                    // get subscripts
+                    kSub = ixMC2/NbrVoxelsSlice;
+                    jSub = (ixMC2 - kSub*NbrVoxelsSlice)/DoseGridSize[1];
+                    iSub = ixMC2 - jSub*DoseGridSize[1] - kSub*NbrVoxelsSlice;
+                    
+                    // flip image
+                    iSub = DoseGridSize[0] - iSub - 1;
+
+                    // compute new index
+                    ixMatRad = jSub + iSub*DoseGridSize[0] + kSub*NbrVoxelsSlice;
+                    
+                    Sparse_index[NbrElements] = ixMatRad;
+                    // if (NbrElements>0)
+                    //   if (Sparse_index[NbrElements]>Sparse_index[NbrElements-1])
+                    //     mexPrintf("i = %d -> ix = %d -> value = %f \n",i,Sparse_index[NbrElements] , Sparse_data[NbrElements]);
                     NbrElements ++;
                 }
             }
@@ -122,6 +165,7 @@ void mexFunction(   int nlhs,   mxArray *plhs[],    int nrhs,   const mxArray *p
     }
     
     Sparse_JC[NbrSpots] = NbrElements;
+    //mexPrintf("ix = %d -> value = %d \n",NbrSpots , NbrElements);
     
     mxFree(data);
     fclose(fid);
