e6d1768d32ad7c77c794388ab904bced3bab3719
Niklas Wahl <niklaswahl2011@gmail.com>
2019-02-13 20:09:03 +0100

    interpolation now also works in octave

--- matRad_calcPhotonDoseMC.m
+++ matRad_calcPhotonDoseMC.m
@@ -182,8 +182,10 @@ end
 
 % downsample ct
 for s = 1:dij.numOfScenarios
-    HUcube{s} =  interp3(dij.ctGrid.x,  dij.ctGrid.y',  dij.ctGrid.z,  ct.cubeHU{s}, ...
-                         dij.doseGrid.x,dij.doseGrid.y',dij.doseGrid.z,'linear');
+    [xct,yct,zct] = meshgrid(dij.ctGrid.x,  dij.ctGrid.y,  dij.ctGrid.z);
+    [xd,yd,zd] = meshgrid(dij.doseGrid.x,dij.doseGrid.y,dij.doseGrid.z);
+    HUcube{s} =  interp3(xct,yct,zct,  ct.cubeHU{s}, ...
+                         xd,yd,zd,'nearest');
 end
 
 %% Setup OmpMC options / parameters
