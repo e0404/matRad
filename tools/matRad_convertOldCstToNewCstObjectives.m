a3d02a6abf51eb50a9ac52ccf4abe21558b6683c
TURING\localbangertm <markbangert@gmail.com>
2019-03-06 16:40:07 +0100

    fix conversion from old cst to new cst

--- tools/matRad_convertOldCstToNewCstObjectives.m
+++ tools/matRad_convertOldCstToNewCstObjectives.m
@@ -18,7 +18,7 @@ function newCst = matRad_convertOldCstToNewCstObjectives(cst)
 
 % %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %
-% Copyright 2015 the matRad development team. 
+% Copyright 2019 the matRad development team. 
 % 
 % This file is part of the matRad project. It is subject to the license 
 % terms in the LICENSE file found in the top-level directory of this 
@@ -29,26 +29,109 @@ function newCst = matRad_convertOldCstToNewCstObjectives(cst)
 %
 % %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-%Copy cst
+% Copy cst
 newCst = cst;
 
-%Loop over cst to convert objectives
-for m=1:size(cst,1)
+% Loop over cst to convert objectives
+for m = 1:size(cst,1)
     if ~isempty(cst{m,6})
         
         %Create empty cell array in the new cst
         newCst{m,6} = cell(0);
         
         %For each objective instanciate the appropriate objective object
-        for n=1:numel(cst{m,6})            
+        for n = 1:numel(cst{m,6})            
+            if isequal(cst{m,6}(n).type,'square deviation')
+                obj = DoseObjectives.matRad_SquaredDeviation;
+                obj.penalty = cst{m,6}(n).penalty;
+                obj.parameters{1} = cst{m,6}(n).dose;
+                newCst{m,6}{n} = obj;
+                
+            elseif isequal(cst{m,6}(n).type,'square overdosing')
+                obj = DoseObjectives.matRad_SquaredOverdosing;
+                obj.penalty = cst{m,6}(n).penalty;
+                obj.parameters{1} = cst{m,6}(n).dose;
+                newCst{m,6}{n} = obj;
+                
+            elseif isequal(cst{m,6}(n).type,'square underdosing')
+                obj = DoseObjectives.matRad_SquaredUnderdosing;
+                obj.penalty = cst{m,6}(n).penalty;
+                obj.parameters{1} = cst{m,6}(n).dose;
+                newCst{m,6}{n} = obj;
+                
+            elseif isequal(cst{m,6}(n).type,'min DVH objective')
+                obj = DoseObjectives.matRad_MinDVH;
+                obj.parameters{1} = cst{m,6}(n).dose;
+                obj.parameters{2} = cst{m,6}(n).volume;
+                newCst{m,6}{n} = obj;
+                
+            elseif isequal(cst{m,6}(n).type,'max DVH objective')
+                obj = DoseObjectives.matRad_MaxDVH;
+                obj.parameters{1} = cst{m,6}(n).dose;
+                obj.parameters{2} = cst{m,6}(n).volume;
+                newCst{m,6}{n} = obj;
+                
+            elseif isequal(cst{m,6}(n).type,'mean')
+                obj = DoseObjectives.matRad_MeanDose;
+                obj.parameters{1} = cst{m,6}(n).dose;
+                newCst{m,6}{n} = obj;
+                
+            elseif isequal(cst{m,6}(n).type,'EUD')
+                obj = DoseObjectives.matRad_EUD;
+                obj.parameters{1} = cst{m,6}(n).dose;
+                obj.parameters{2} = cst{m,6}(n).EUD;                
+                newCst{m,6}{n} = obj;
             
+            %Constraints
+            elseif isequal(cst{m,6}(n).type, 'max dose constraint')
+                obj = DoseConstraints.matRad_MinMaxDose;
+                obj.parameters{1} = 0;
+                obj.parameters{2} = cst{m,6}(n).dose;
+            
+            elseif isequal(constraint.type, 'min dose constraint')
+                obj = DoseConstraints.matRad_MinMaxDose;
+                obj.parameters{1} = cst{m,6}(n).dose;
+                obj.parameters{2} = Inf;
+
+            elseif isequal(constraint.type, 'min mean dose constraint')
+                obj = DoseConstraints.matRad_MinMaxMeanDose;
+                obj.parameters{1} = cst{m,6}(n).dose;
+                obj.parameters{2} = Inf;
+                
+            elseif isequal(constraint.type, 'max mean dose constraint') 
+                obj = DoseConstraints.matRad_MinMaxMeanDose;
+                obj.parameters{1} = 0;
+                obj.parameters{2} = cst{m,6}(n).dose;
+
+
+            elseif isequal(constraint.type, 'min EUD constraint')
+                obj = DoseConstraints.matRad_MinMaxEUD;
+                obj.parameters{1} = cst{m,6}(n).EUD;
+                obj.parameters{2} = cst{m,6}(n).dose;
+                obj.parameters{3} = Inf;
+                
+            elseif isequal(constraint.type, 'max EUD constraint')
+                obj = DoseConstraints.matRad_MinMaxEUD;
+                obj.parameters{1} = cst{m,6}(n).EUD;
+                obj.parameters{2} = 0;
+                obj.parameters{3} = cst{m,6}(n).dose;
+        
+
+            elseif isequal(constraint.type, 'max DVH constraint')
+                obj = DoseConstraints.matRad_MinMaxDVH;
+                obj.parameters{1} = cst{m,6}(n).dose;
+                obj.parameters{2} = 0;
+                obj.parameters{3} = cst{m,6}(n).volume;
+            
+            elseif isequal(constraint.type, 'min DVH constraint')             
+                obj = DoseConstraints.matRad_MinMaxDVH;
+                obj.parameters{1} = cst{m,6}(n).dose;
+                obj.parameters{2} = cst{m,6}(n).volume;
+                obj.parameters{3} = 1;
+            else
+                warndlg('ERROR. Can not convert CST objectives/constraints.','Loading Error');
+                break;
+            end
         end
     end
 end
-
-
-
-
-
-
-
