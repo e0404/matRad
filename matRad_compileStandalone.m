dbd0ea5181eb8f26dbdeb341fc311028d8c4419c
nellyabbani <n.abbani@dkfz-heidelberg.de>
2020-02-04 17:49:36 +0100

    standalone script

--- matRad_compileStandalone.m
+++ matRad_compileStandalone.m
@@ -2,12 +2,15 @@
 
 if ismac
     suffix = 'Mac';
+    installSuffix = 'dmg';
     % Mac platform
 elseif isunix
     suffix = 'Linux';
+    installSuffix = 'install';
     % Linux platform
 elseif ispc
     suffix = 'Windows';
+    installSuffix = 'exe';
     % Windows platform
 else
     error('Platform not supported')
@@ -16,34 +19,12 @@ end
 
 
 FileName=['matRad' suffix '.prj'];
+
 xDoc = xmlread(FileName);
 
-%compile without runtime
-xDoc.getElementsByTagName('param.web.mcr').item(0).getFirstChild.setData('true');
-xDoc.getElementsByTagName('param.package.mcr').item(0).getFirstChild.setData('false');
-
-xmlwrite(FileName,xDoc);
-
-clc
-applicationCompiler('-package',FileName);
-
-cmdWinDoc = com.mathworks.mde.cmdwin.CmdWinDocument.getInstance;
-% loop until 'Package finished' or 'Package failed' is found in the command window
-tic
-while toc<500
-  pause ( 2 )
-  myTxt = cmdWinDoc.getText(cmdWinDoc.getStartPosition.getOffset,cmdWinDoc.getLength);
-  %
-  if ~isempty ( strfind ( myTxt, 'Package finished' ) )
-    break
-  end
-  if ~isempty ( strfind ( myTxt, 'Package failed' ) )
-    break
-  end
-end
-
-disp('Packaged compiler without runtime');
-
+%Get Filenames
+filename_webRT = xDoc.getElementsByTagName('param.web.mcr.name').item(0).getFirstChild.getData().toCharArray()';
+filename_withRT = xDoc.getElementsByTagName('param.package.mcr.name').item(0).getFirstChild.getData().toCharArray()';
 
 %compile with runtime
 xDoc.getElementsByTagName('param.web.mcr').item(0).getFirstChild.setData('false');
@@ -57,19 +38,50 @@ applicationCompiler('-package',FileName);
 cmdWinDoc = com.mathworks.mde.cmdwin.CmdWinDocument.getInstance;
 % loop until 'Package finished' or 'Package failed' is found in the command window
 tic
+worked = false;
 while toc<500
   pause ( 2 )
-  myTxt = cmdWinDoc.getText(cmdWinDoc.getStartPosition.getOffset,cmdWinDoc.getLength);
+  %myTxt = cmdWinDoc.getText(cmdWinDoc.getStartPosition.getOffset,cmdWinDoc.getLength);
   %
-  if ~isempty ( strfind ( myTxt, 'Package finished' ) )
-    break
+  if exist(['standalone' filesep 'for_redistribution' filesep  filename_withRT '.' installSuffix], 'file') == 2
+      worked = true;
+      break
   end
-  if ~isempty ( strfind ( myTxt, 'Package failed' ) )
-    break
+end
+if worked == false
+    error('Packaging Failed!');
+end
+disp('Packaged compiler with runtime');
+
+%compile without runtime
+xDoc.getElementsByTagName('param.web.mcr').item(0).getFirstChild.setData('true');
+xDoc.getElementsByTagName('param.package.mcr').item(0).getFirstChild.setData('false');
+
+xmlwrite(FileName,xDoc);
+
+clc
+applicationCompiler('-package',FileName);
+
+%cmdWinDoc = com.mathworks.mde.cmdwin.CmdWinDocument.getInstance;
+% loop until 'Package finished' or 'Package failed' is found in the command window
+tic
+worked = false;
+while toc<500
+  pause ( 2 )
+  %myTxt = cmdWinDoc.getText(cmdWinDoc.getStartPosition.getOffset,cmdWinDoc.getLength);
+  %
+  if exist(['standalone' filesep 'for_redistribution' filesep  filename_webRT '.' installSuffix], 'file') == 2
+      worked = true;
+      break
   end
 end
 
-disp('Packaged both compilers');
+if worked == false
+    error('Packaging Failed!');
+end
+disp('Packaged compiler without runtime');
 
+
+disp('Packaged both compilers');
 
 
