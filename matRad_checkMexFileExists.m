4af81b919a496d7c60f544c9fb190e2bb8ee4b93
nellyabbani <n.abbani@dkfz-heidelberg.de>
2020-02-27 13:47:54 +0100

    Update function to check mex files for octave

--- matRad_checkMexFileExists.m
+++ matRad_checkMexFileExists.m
@@ -1,10 +1,36 @@
-function fileExists = matRad_checkMexFileExists(filename) 
-%UNTITLED Summary of this function goes here
-%   Detailed explanation goes here
+function fileExists = matRad_checkMexFileExists(filename,noLinkOctave) 
+% Checks if a matching mex file exists, and creates a link if a matching 
+% octave mex file is found
+%
+% call
+%   matRad_checkMexFileExists(filename)
+%
+%
+% References
+%
+%
+% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%
+% Copyright 2020 the matRad development team. 
+% 
+% This file is part of the matRad project. It is subject to the license 
+% terms in the LICENSE file found in the top-level directory of this 
+% distribution and at https://github.com/e0404/matRad/LICENSES.txt. No part 
+% of the matRad project, including this file, may be copied, modified, 
+% propagated, or distributed except according to the terms contained in the 
+% LICENSE file.
+%
+% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
   
+%Explicitly check for matching mex file (id 3)
 fileExists = (exist(filename,'file') == 3);
 
-if ~fileExists && strcmp(matRad_getEnvironment(),'OCTAVE')
+if nargin < 2
+    noLinkOctave = false;
+end
+
+if ~fileExists && strcmp(matRad_getEnvironment(),'OCTAVE') && ~noLinkOctave
     
     [~,maxArraySize] = computer();
     
@@ -26,7 +52,7 @@ if ~fileExists && strcmp(matRad_getEnvironment(),'OCTAVE')
     
     octfilename = [filename '.' systemext bitExt];
 
-    %Check if file exists
+    %Check if matching octave file exists
     fileExists = (exist(octfilename,'file') == 2);
     
     if fileExists
