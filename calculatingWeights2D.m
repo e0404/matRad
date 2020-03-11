916cd04abac771c2176270e639a8df25b6324569
pamede <p.meder@protonmail.com>
2020-03-11 10:20:00 -0300

    updated calculating weights

--- calculatingWeights2D.m
+++ calculatingWeights2D.m
@@ -1,16 +1,18 @@
 sigmaTot = 5;
 sigmaSub = 2;
-N = 31;
+N = 5;
 
 sigmaHead = sqrt(sigmaTot^2 - sigmaSub^2);
 deltaR = (7 * sigmaHead) / N;
 
 weight = @(mu, deltaR, i) integral(@(r) normpdf(r,mu,sigmaHead) , mu+(i-0.5)*deltaR, mu+(i+0.5)*deltaR);
 
-range = -15:0.1:15;
+range = -15:0.5:15;
 sum = zeros(size(range));
+allWeights = [];
 for i = -(N - 1) / 2 : (N - 1) / 2
     w = weight(0, deltaR, i);
+    allWeights  = [allWeights, w];
     plot(range, w * normpdf(range, i*deltaR, sigmaSub))
     sum = sum + w * normpdf(range, i*deltaR, sigmaSub);
     hold on
@@ -18,3 +20,9 @@ end
 plot(range, sum)
 plot(range, normpdf(range,0,sigmaTot));
 hold off
+
+radialGauss = @(r, sigma) 1 / (2 * pi * sigma^2) * exp(-0.5 * (r/sigma)^2);
+[X,Y] = meshgrid(range,range);
+ergGauss = arrayfun(@(x,y) radialGauss(sqrt(x^2 + y^2),sigmaTot),X,Y);
+surf(ergGauss)
+
