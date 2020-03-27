b4e0cfa399fb640928c28de067d50dbae06d189c
pamede <p.meder@protonmail.com>
2020-03-27 17:07:44 +0100

    new data structure for MC base data and APM depth dose calculation was taken out

--- matRad_calcParticleDoseBixel.m
+++ matRad_calcParticleDoseBixel.m
@@ -36,81 +36,37 @@ depths = baseData.depths + baseData.offset;
 % convert from MeV cm^2/g per primary to Gy mm^2 per 1e6 primaries
 conversionFactor = 1.6021766208e-02;
 
-if ~isstruct(baseData.Z)
-    if ~isfield(baseData,'sigma')
-        % interpolate depth dose, sigmas, and weights    
-        X = matRad_interp1(depths,[conversionFactor*baseData.Z baseData.sigma1 baseData.weight baseData.sigma2],radDepths);
-
-
-
-        % set dose for query > tabulated depth dose values to zero
-        X(radDepths > max(depths),1) = 0;
-
-        % compute lateral sigmas
-        sigmaSq_Narr = X(:,2).^2 + sigmaIni_sq;
-        sigmaSq_Bro  = X(:,4).^2 + sigmaIni_sq;
-
-        % calculate lateral profile
-        L_Narr =  exp( -radialDist_sq ./ (2*sigmaSq_Narr))./(2*pi*sigmaSq_Narr);
-        L_Bro  =  exp( -radialDist_sq ./ (2*sigmaSq_Bro ))./(2*pi*sigmaSq_Bro );
-        L = baseData.LatCutOff.CompFac * ((1-X(:,3)).*L_Narr + X(:,3).*L_Bro);
-
-        dose = X(:,1).*L;
-    else
-
-        % interpolate depth dose and sigma
-        X = matRad_interp1(depths,[conversionFactor*baseData.Z baseData.sigma],radDepths);
-
-        %compute lateral sigma
-        sigmaSq = X(:,2).^2 + sigmaIni_sq;
-
-        % calculate dose
-        dose = baseData.LatCutOff.CompFac * exp( -radialDist_sq ./ (2*sigmaSq)) .* X(:,1) ./(2*pi*sigmaSq);
-
-     end
-else
-    % function handle for calculating lateral dose
-    Gauss    = @(x,mu,SqSigma) 1./(sqrt(2*pi.*SqSigma)).*exp(-((x - mu).^2./(2.*SqSigma)));
-
-    % function handle for calculating depth doses
-    sumGauss = @(x,mu,SqSigma,w) (1./sqrt(2*pi*ones(numel(x),1) .* SqSigma') .* ...
-        exp(-bsxfun(@minus,x,mu').^2 ./ (2* ones(numel(x),1) .* SqSigma' ))) * w;
+if ~isfield(baseData,'sigma')
     
-    if ~isfield(baseData,'sigma')
+    % interpolate depth dose, sigmas, and weights    
+    X = matRad_interp1(depths,[conversionFactor*baseData.Z baseData.sigma1 baseData.weight baseData.sigma2],radDepths);
+    
+    % set dose for query > tabulated depth dose values to zero
+    X(radDepths > max(depths),1) = 0;
         
-        % interpolate depth dose, sigmas, and weights    
-        %X = matRad_interp1(depths,[conversionFactor*baseData.Z.profileORG baseData.sigma1 baseData.weight baseData.sigma2],radDepths);
-        X = matRad_interp1(depths,[baseData.sigma1 baseData.weight baseData.sigma2],radDepths);
-        
-        X = [conversionFactor * sumGauss(radDepths,baseData.Z.mean,baseData.Z.width.^2,baseData.Z.weight), X];
+    % compute lateral sigmas
+    sigmaSq_Narr = X(:,2).^2 + sigmaIni_sq;
+    sigmaSq_Bro  = X(:,4).^2 + sigmaIni_sq;
+    
+    % calculate lateral profile
+    L_Narr =  exp( -radialDist_sq ./ (2*sigmaSq_Narr))./(2*pi*sigmaSq_Narr);
+    L_Bro  =  exp( -radialDist_sq ./ (2*sigmaSq_Bro ))./(2*pi*sigmaSq_Bro );
+    L = baseData.LatCutOff.CompFac * ((1-X(:,3)).*L_Narr + X(:,3).*L_Bro);
+
+    dose = X(:,1).*L;
+else
+    
+    % interpolate depth dose and sigma
+    X = matRad_interp1(depths,[conversionFactor*baseData.Z baseData.sigma],radDepths);
+
+    %compute lateral sigma
+    sigmaSq = X(:,2).^2 + sigmaIni_sq;
+    
+    % calculate dose
+    dose = baseData.LatCutOff.CompFac * exp( -radialDist_sq ./ (2*sigmaSq)) .* X(:,1) ./(2*pi*sigmaSq);
+    
+ end
  
-        % set dose for query > tabulated depth dose values to zero
-        X(radDepths > max(depths),1) = 0;
-
-        % compute lateral sigmas
-        sigmaSq_Narr = X(:,2).^2 + sigmaIni_sq;
-        sigmaSq_Bro  = X(:,4).^2 + sigmaIni_sq;
-
-        % calculate lateral profile
-        L_Narr =  exp( -radialDist_sq ./ (2*sigmaSq_Narr))./(2*pi*sigmaSq_Narr);
-        L_Bro  =  exp( -radialDist_sq ./ (2*sigmaSq_Bro ))./(2*pi*sigmaSq_Bro );
-        L = baseData.LatCutOff.CompFac * ((1-X(:,3)).*L_Narr + X(:,3).*L_Bro);
-
-        dose = X(:,1).*L;
-    else
-
-        % interpolate depth dose and sigma
-        X = matRad_interp1(depths,[conversionFactor*baseData.Z baseData.sigma],radDepths);
-
-        %compute lateral sigma
-        sigmaSq = X(:,2).^2 + sigmaIni_sq;
-
-        % calculate dose
-        dose = baseData.LatCutOff.CompFac * exp( -radialDist_sq ./ (2*sigmaSq)) .* X(:,1) ./(2*pi*sigmaSq);
-
-    end
-end
-
 % check if we have valid dose values
 if any(isnan(dose)) || any(dose<0)
    error('Error in particle dose calculation.');
