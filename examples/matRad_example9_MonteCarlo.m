66a19a72db9261e3423b8f1c0184e61b19b04413
Niklas Wahl <niklaswahl2011@gmail.com>
2020-10-06 15:42:31 +0200

    default engine MCsquare

--- examples/matRad_example9_MonteCarlo.m
+++ examples/matRad_example9_MonteCarlo.m
@@ -52,8 +52,9 @@ pln.propOpt.bioOptimization = 'none'; % none: physical optimization;            
 pln.propOpt.runDAO          = false;  % 1/true: run DAO, 0/false: don't / will be ignored for particles
 pln.propOpt.runSequencing   = false;  % 1/true: run sequencing, 0/false: don't / will be ignored for particles and also triggered by runDAO below
 
-pln.propMC.proton_engine = 'TOPAS';
-%pln.propMC.proton_engine = 'MCsquare';
+pln.propMC.proton_engine = 'MCsquare';
+%pln.propMC.proton_engine = 'TOPAS';
+
 
 %% generate steering file
 stf = matRad_generateStf(ct,cst,pln);
