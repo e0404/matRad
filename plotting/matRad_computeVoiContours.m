e8596692beba0d74257891b551e9869dab96d983
Niklas Wahl <niklaswahl2011@gmail.com>
2020-11-06 14:02:57 +0100

    Fix progress count in precomputing VOI contours

--- plotting/matRad_computeVoiContours.m
+++ plotting/matRad_computeVoiContours.m
@@ -48,6 +48,6 @@ for ctScen = 1:ct.numOfCtScen
                 cst{s,7}{1,ctScen}{slice,3} = contourc(squeeze(mask(:,:,slice)),.5*[1 1]);
             end
         end
-        matRad_progress(s + ctScen*size(cst,1),size(cst,1)*ct.numOfCtScen);
+        matRad_progress(s + (ctScen-1)*s,size(cst,1)*ct.numOfCtScen);
     end
 end
