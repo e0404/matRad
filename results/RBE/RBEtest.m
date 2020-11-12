fda9b568cc378e03294e4384ed1db93d32b44346
homolka <n.homolka@dkfz.de>
2020-11-12 10:44:05 +0100

    changes in RBE testing script

--- results/RBE/RBEtest.m
+++ results/RBE/RBEtest.m
@@ -1,4 +1,4 @@
-clear
+% clear
 
 % Patient Data Import
 load('BOXPHANTOM_LUNG_LARGE.mat');
@@ -11,7 +11,9 @@ pln.machine         = 'Generic';
 
 % modelName           = 'LEM';
 modelName           = 'MCN';
-quantityOpt         = 'RBExD';
+% quantityOpt         = 'RBExD';
+quantityOpt         = 'physicalDose';
+
 
 pln.propOpt.bioOptimization = 'none';
 
@@ -34,22 +36,34 @@ pln.multScen = matRad_multScen(ct,'nomScen'); % optimize on the nominal scenario
 
 pln.heterogeneity.calcHetero = false;
 pln.heterogeneity.useOriginalDepths = false;
-pln.propDoseCalc.airOffsetCorrection = false;
+pln.propDoseCalc.airOffsetCorrection = true;
 %
 stf = matRad_generateStfPencilBeam(pln,ct);
 % stf = matRad_generateStf(ct,cst,pln);
-
+weights = ones(1,pln.propStf.numOfBeams);
+% %%
+% dijMatRad = matRad_calcParticleDose(ct,stf,pln,cst,1);
+% dijTOPAS = matRad_calcParticleDoseMCtopas(ct,stf,pln,cst,1e4,1);
 %%
-% dijMatRad = matRad_calcParticleDose(ct,stf,pln,cst);
-
-%%
-resultGUI_matRad = matRad_calcDoseDirect(ct,stf,pln,cst,1);
+resultGUI_matRad = matRad_calcDoseDirect(ct,stf,pln,cst,weights);
 
 %% TOPAS heterogeneity correction
+pln.propMC.proton_engine = 'MCsquare';
+resultGUI_MCsquare = matRad_calcDoseDirectMC(ct,stf,pln,cst,1,1e4);
+%%
 pln.propMC.proton_engine = 'TOPAS';
-resultGUI_TOPAS = matRad_calcDoseDirectMC(ct,stf,pln,cst,0.001,1e2);
+tic
+resultGUI_TOPAS = matRad_calcDoseDirectMC(ct,stf,pln,cst,1,1e4);
+toc
 %%
 % resultGUI_TOPAS_RBE = matRad_calcDoseDirectMC(ct,stf,pln,cst,weights,1e5);
 %%
 % dijTOPAS = matRad_calcParticleDoseMCtopas(ct,stf,pln,cst);
 % resultGUI_TOPAS = matRad_fluenceOptimization(dijTOPAS,cst,pln);
+
+figure, plot(matRad_calcIDD(resultGUI_matRad.physicalDose)), hold on,...
+    plot(matRad_calcIDD(resultGUI_MCsquare.physicalDose)),...
+    plot(matRad_calcIDD(resultGUI_TOPAS.physicalDose))
+legend({'matRad','MCsquare','TOPAS'},'Location','northwest')
+xlim([10 100])
+ylabel('proton physicalDose')
