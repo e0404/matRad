7a0286142a7ccef5be282b95133c77b992d8f2f6
homolka <n.homolka@dkfz.de>
2021-02-18 16:52:12 +0100

    can now read multiple folders from openstack for heterogeneity sampling

--- topas/matRad_readOpenStack.m
+++ topas/matRad_readOpenStack.m
@@ -1,36 +1,50 @@
 function outDose = matRad_readOpenStack(folder)
 
-topasConfig = MatRad_TopasConfig();
-topasCubes = matRad_readTopasData(folder);
-
-load([folder filesep 'dij.mat']);
-load([folder filesep 'weights.mat']);
-
-ctScen = 1;
-fnames = fieldnames(topasCubes);
-dij.MC_tallies = fnames;
-
-if ~isfield(topasCubes,'RBE')
-    for f = 1:numel(fnames)
-        dij.(fnames{f}){ctScen,1} = sum(w(:,ctScen))*reshape(topasCubes.(fnames{f}),[],1);
+if contains(folder,'*')% && all(modulation ~= false)
+    folder = dir(folder);
+    for i = 1:length(folder)
+        folders{i} = [folder(i).folder filesep folder(i).name];
     end
 else
-    for d = 1:length(stf)
-        dij.physicalDose{ctScen,1}(:,d)    = sum(w)*reshape(topasCubes.(['physicalDose_beam',num2str(d)]),[],1);
-        dij.alpha{ctScen,1}(:,d)           = reshape(topasCubes.(['alpha_beam',num2str(d)]),[],1);
-        dij.beta{ctScen,1}(:,d)            = reshape(topasCubes.(['beta_beam',num2str(d)]),[],1);
-        %                             dij.RBE{1}(:,d)             = reshape(topasCubes.(['RBE_beam',num2str(d)]),[],1);
-        
-        [dij.ax,dij.bx] = matRad_getPhotonLQMParameters(cst,prod(ct.cubeDim),1);
-        %                             dij.ax = full(reshape(ax,ct.cubeDim));
-        %                             dij.bx = full(reshape(bx,ct.cubeDim));
-        dij.abx(dij.bx>0) = dij.ax(dij.bx>0)./dij.bx(dij.bx>0);
-        
-        dij.mAlphaDose{ctScen,1}(:,d)      = dij.physicalDose{ctScen,1}(:,d) .* dij.alpha{ctScen,1}(:,d);
-        dij.mSqrtBetaDose{ctScen,1}(:,d)   = sqrt(dij.physicalDose{ctScen,1}(:,d)) .* dij.beta{ctScen,1}(:,d);
-    end
+    folders{1} = folder;
 end
 
-resultGUI    = matRad_calcCubes(ones(dij.numOfBeams,1),dij,1);
-outDose = resultGUI.physicalDose;
+
+for f = 1:length(folders)
+    currFolder = folders{f};
+    topasConfig = MatRad_TopasConfig();
+    topasCubes = matRad_readTopasData(currFolder);
+    
+    load([currFolder filesep 'dij.mat']);
+    load([currFolder filesep 'weights.mat']);
+    
+    ctScen = 1;
+    fnames = fieldnames(topasCubes);
+    dij.MC_tallies = fnames;
+    
+    if ~isfield(topasCubes,'RBE')
+        for f = 1:numel(fnames)
+            dij.(fnames{f}){ctScen,1} = sum(w(:,ctScen))*reshape(topasCubes.(fnames{f}),[],1);
+        end
+    else
+        for d = 1:length(stf)
+            dij.physicalDose{ctScen,1}(:,d)    = sum(w)*reshape(topasCubes.(['physicalDose_beam',num2str(d)]),[],1);
+            dij.alpha{ctScen,1}(:,d)           = reshape(topasCubes.(['alpha_beam',num2str(d)]),[],1);
+            dij.beta{ctScen,1}(:,d)            = reshape(topasCubes.(['beta_beam',num2str(d)]),[],1);
+            
+            [dij.ax,dij.bx] = matRad_getPhotonLQMParameters(cst,prod(ct.cubeDim),1);
+            dij.abx(dij.bx>0) = dij.ax(dij.bx>0)./dij.bx(dij.bx>0);
+            
+            dij.mAlphaDose{ctScen,1}(:,d)      = dij.physicalDose{ctScen,1}(:,d) .* dij.alpha{ctScen,1}(:,d);
+            dij.mSqrtBetaDose{ctScen,1}(:,d)   = sqrt(dij.physicalDose{ctScen,1}(:,d)) .* dij.beta{ctScen,1}(:,d);
+        end
+    end
+    
+    resultGUI    = matRad_calcCubes(w,dij,1);
+    
+    if ~exist('outDose','var')
+        outDose = zeros(size(resultGUI.physicalDose));
+    end
+        
+    outDose = outDose + resultGUI.physicalDose/length(folders);
 end
