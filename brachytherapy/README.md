d2a510dbeba26bab436c27ef5fd74679b4213153
Copani <82407657+Copani@users.noreply.github.com>
2021-04-28 14:30:53 +0200

    MatRad Brachytherapy
    An attempt to develop a brachytherapy planing tool using the matRad framework.
    
    For now, everything is stored in an extra folder called "brachytherapy"

---
+++ brachytherapy/README.md
@@ -0,0 +1,2 @@
+This is a commodity for life brachytherapy oriented at the matRad Structures. 
+To keep track of used functions, everything is stored in this folder.
---
+++ brachytherapy/matRad_Brachy.m
@@ -0,0 +1,88 @@
+% matRad script for Brachytherapy
+%
+%
+% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%
+% Copyright 2015 the matRad development team. 
+% 
+% This file is part of the matRad project. It is subject to the license 
+% terms in the LICENSE file found in the top-level directory of this 
+% distribution and at https://github.com/e0404/matRad/LICENSES.txt. No part 
+% of the matRad project, including this file, may be copied, modified, 
+% propagated, or distributed except according to the terms contained in the 
+% LICENSE file.
+%
+% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+matRad_rc
+
+% load patient data, i.e. ct, voi, cst
+
+%load HEAD_AND_NECK
+load TG119.mat
+%load PROSTATE.mat
+%load LIVER.mat
+%load BOXPHANTOM.mat
+
+% meta information for treatment plan
+
+pln.radiationMode   = 'photons';     % either photons / protons / carbon
+pln.machine         = 'Generic';
+
+pln.numOfFractions  = 30;
+
+% beam geometry settings
+pln.propStf.bixelWidth      = 5; % [mm] / also corresponds to lateral spot spacing for particles
+pln.propStf.gantryAngles    = [0:72:359]; % [?]
+pln.propStf.couchAngles     = [0 0 0 0 0]; % [?]
+pln.propStf.numOfBeams      = numel(pln.propStf.gantryAngles);
+pln.propStf.isoCenter       = ones(pln.propStf.numOfBeams,1) * matRad_getIsoCenter(cst,ct,0);
+
+% dose calculation settings
+pln.propDoseCalc.doseGrid.resolution.x = 5; % [mm]
+pln.propDoseCalc.doseGrid.resolution.y = 5; % [mm]
+pln.propDoseCalc.doseGrid.resolution.z = 5; % [mm]
+
+% optimization settings
+pln.propOpt.optimizer       = 'IPOPT';
+pln.propOpt.bioOptimization = 'none'; % none: physical optimization;             const_RBExD; constant RBE of 1.1;
+                                      % LEMIV_effect: effect-based optimization; LEMIV_RBExD: optimization of RBE-weighted dose
+pln.propOpt.runDAO          = false;  % 1/true: run DAO, 0/false: don't / will be ignored for particles
+pln.propOpt.runSequencing   = false;  % 1/true: run sequencing, 0/false: don't / will be ignored for particles and also triggered by runDAO below
+
+%% initial visualization and change objective function settings if desired
+matRadGUI
+
+%% generate steering file
+stf = matRad_generateStf(ct,cst,pln);
+
+%% dose calculation
+if strcmp(pln.radiationMode,'photons')
+    dij = matRad_calcPhotonDose(ct,stf,pln,cst);
+    %dij = matRad_calcPhotonDoseVmc(ct,stf,pln,cst);
+elseif strcmp(pln.radiationMode,'protons') || strcmp(pln.radiationMode,'carbon')
+    dij = matRad_calcParticleDose(ct,stf,pln,cst);
+end
+
+%% inverse planning for imrt
+resultGUI = matRad_fluenceOptimization(dij,cst,pln);
+
+%% sequencing
+if strcmp(pln.radiationMode,'photons') && (pln.propOpt.runSequencing || pln.propOpt.runDAO)
+    %resultGUI = matRad_xiaLeafSequencing(resultGUI,stf,dij,5);
+    %resultGUI = matRad_engelLeafSequencing(resultGUI,stf,dij,5);
+    resultGUI = matRad_siochiLeafSequencing(resultGUI,stf,dij,5);
+end
+
+%% DAO
+if strcmp(pln.radiationMode,'photons') && pln.propOpt.runDAO
+   resultGUI = matRad_directApertureOptimization(dij,cst,resultGUI.apertureInfo,resultGUI,pln);
+   matRad_visApertureInfo(resultGUI.apertureInfo);
+end
+
+%% start gui for visualization of result
+matRadGUI
+
+%% indicator calculation and show DVH and QI
+[dvh,qi] = matRad_indicatorWrapper(cst,pln,resultGUI);
+
