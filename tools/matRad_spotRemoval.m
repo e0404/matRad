f3d656c84de6038fe0363bffeb5f72de39a21825
homolka <n.homolka@dkfz.de>
2021-08-09 23:02:55 +0200

    added function that removes underweighted spots for faster calculation

---
+++ tools/matRad_spotRemoval.m
@@ -0,0 +1,92 @@
+function [dij,stf] = matRad_spotRemoval(dij,w,stf)
+% matRad postprosseing function accounting for
+%       minimum number of particles per spot
+%       minimum number of particles per iso-energy slice
+%
+% call
+%   resultGUI =  matRad_postprocessing(resultGUI, dij, pln, cst, stf)
+
+% input
+%   resultGUI   struct containing optimized fluence vector
+%   dij:        matRad dij struct
+%   pln:        matRad pln struct
+%   cst:        matRad cst struct
+%   stf:        matRad stf struct
+%
+% output
+%   resultGUI:  new w and doses in resultGUI
+%
+%
+% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%
+% Copyright 2018 the matRad development team.
+%
+% This file is part of the matRad project. It is subject to the license
+% terms in the LICENSE file found in the top-level directory of this
+% distribution and at https://github.com/e0404/matRad/LICENSES.txt. No part
+% of the matRad project, including this file, may be copied, modified,
+% propagated, or distributed except according to the terms contained in the
+% LICENSE file.
+%
+% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+matRad_cfg = MatRad_Config.instance();
+
+if nargin < 3 && nargout > 1
+    matRad_cfg.dispError('stf is missing as input')
+end
+
+% set threshold for spot removal to 0.5% of the maximum weight.
+thres = 0.005;
+newSpots = w>thres*max(w);
+
+if (sum(newSpots) ~= numel(w)) && sum(newSpots) ~= dij.totalNumOfBixels
+    dij.cutWeights = w(newSpots);
+    
+    dij.bixelNum = dij.bixelNum(newSpots);
+    dij.rayNum = dij.rayNum(newSpots);
+    dij.beamNum = dij.beamNum(newSpots);
+    dij.totalNumOfBixels = sum(newSpots);
+    
+    dij.physicalDose{1} = dij.physicalDose{1}(:,newSpots);
+    if isfield(dij,'mAlphaDose')
+        dij.mAlphaDose{1} = dij.mAlphaDose{1}(:,newSpots);
+        dij.mSqrtBetaDose{1} = dij.mSqrtBetaDose{1}(:,newSpots);
+    end
+    
+    [~,beamNumIdx] = unique(dij.beamNum);
+    beamNumIdx = [0;beamNumIdx(2:end)-1;dij.totalNumOfBixels];
+    
+    for b = 1:dij.numOfBeams
+        [rayCnt,rayIdx] = unique(dij.rayNum(beamNumIdx(b)+1:beamNumIdx(b+1)));
+        dij.numOfRaysPerBeam(b) = numel(rayCnt);
+        
+        if exist('stf')
+            numOfBixelsPerRay = groupcounts(dij.rayNum(beamNumIdx(b)+1:beamNumIdx(b+1)));
+            cutRays = ismember([1:dij.numOfRaysPerBeam(b)]',rayCnt);
+            if any(~cutRays)
+                stf(b).ray = stf(b).ray(cutRays);
+                stf(b).numOfRays = sum(cutRays);
+            end
+            bixelsCurrRay = dij.bixelNum(beamNumIdx(b)+1:beamNumIdx(b+1));
+            for i = 1:stf(b).numOfRays
+                bixelCurrRay{i} = bixelsCurrRay(rayIdx(i):rayIdx(i)+numOfBixelsPerRay(i)-1);
+            end
+            for f = 1:stf(b).numOfRays
+                stf(b).ray(f).energy = stf(b).ray(f).energy(bixelCurrRay{f});
+                stf(b).ray(f).focusIx = stf(b).ray(f).focusIx(bixelCurrRay{f});
+                stf(b).ray(f).rangeShifter = stf(b).ray(f).rangeShifter(bixelCurrRay{f});
+            end
+            stf(b).numOfBixelsPerRay = numOfBixelsPerRay';
+            stf(b).totalNumOfBixels = sum(numOfBixelsPerRay);
+        end
+    end
+    
+    dij.totalNumOfRays = sum(dij.numOfRaysPerBeam);
+    matRad_cfg.dispInfo([num2str(sum(~newSpots)),' spots have been removed below ',num2str(100*thres),'%%.\n'])
+else
+    matRad_cfg.dispWarning('no spots have been removed.')
+    dij.cutWeights = w;
+end
+end
+
