a0de03e4fddb26e10b7163c336f379072407467c
Noa Homolka <n.homolka@dkfz.de>
2021-09-21 20:36:14 +0200

    PRIVATE, EDITED: plotIDD function more robust

--- plotting/plotIDD.m
+++ plotting/plotIDD.m
@@ -4,18 +4,35 @@ function plotIDD(varargin)
 figure
 hold on
 
-if isstr(varargin{end}) && (strcmp(varargin{end},'RBE') || strcmp(varargin{end},'RBExD'))
-    for i = 1:numel(varargin)-1
-        plot(matRad_calcIDD(varargin{i}.RBExD,1),'LineWidth',1.5, 'DisplayName', inputname(i))
-    end
-    ylabel('RBExD')
+if ~isstruct(varargin{end-1})
+    profile = varargin{end-1};
+    until = numel(varargin)-2;
 else
-    for i = 1:numel(varargin)
-        plot(matRad_calcIDD(varargin{i}.physicalDose,1),'LineWidth',1.5, 'DisplayName', inputname(i))
+    profile = 0;
+    until = numel(varargin)-1;
+end
+
+if isstr(varargin{end}) && (strcmp(varargin{end},'RBE') || strcmp(varargin{end},'RBExD'))
+    for i = 1:until
+        plot(matRad_calcIDD(varargin{i}.RBExD,profile),'LineWidth',1.5, 'DisplayName', inputname(i))
     end
-    ylabel('physicalDose')
+if profile
+    ylabel('profile / RBExD')
+ else   
+    ylabel('IDD / RBExD')
+    end
+else
+    for i = 1:until+1
+        plot(matRad_calcIDD(varargin{i}.physicalDose,profile),'LineWidth',1.5, 'DisplayName', inputname(i))
+    end
+    if profile
+    ylabel('profile / physicalDose')
+ else   
+    ylabel('IDD / physicalDose')
+    end
 end
 xlabel('depth [voxel]')
+
 % xlim([0 size(varargin{i}.physicalDose,1)*0.8])
 legend('Interpreter', 'none','Location','northwest')
 end
