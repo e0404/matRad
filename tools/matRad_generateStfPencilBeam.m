f226e7847be7864a66efa0d65dfb6eba390bfb3f
Noa Homolka <n.homolka@dkfz.de>
2021-10-21 05:05:28 +0200

    implemented multiple pencil beams in one plane to get better statistic in z direction

--- tools/matRad_generateStfPencilBeam.m
+++ tools/matRad_generateStfPencilBeam.m
@@ -1,4 +1,4 @@
-function stf = matRad_generateStfPencilBeam(pln,ct,energyIx)
+function stf = matRad_generateStfPencilBeam(pln,ct,energyIx,plane)
 % function to generate very basic stf file for a single energy
 %
 % call
@@ -36,6 +36,10 @@ if numel(pln.propStf.gantryAngles) ~= numel(pln.propStf.couchAngles)
     matRad_cfg.dispError('Inconsistent number of gantry and couch angles.');
 end
 
+if nargin < 4
+    plane = false;
+end
+
 SAD = machine.meta.SAD;
 % pln.propStf.bixelWidth = 5;
 currentMinimumFWHM = matRad_interp1(machine.meta.LUT_bxWidthminFWHM(1,:)', machine.meta.LUT_bxWidthminFWHM(2,:)',pln.propStf.bixelWidth);
@@ -52,17 +56,8 @@ for i = 1:length(pln.propStf.gantryAngles)
     stf(i).SAD = SAD;
     stf(i).isoCenter = pln.propStf.isoCenter(i,:);
     
-    stf(i).numOfRays = 1;
-    stf(i).numOfBixelsPerRay = 1;
-    stf(i).totalNumOfBixels = 1;
-    
     stf(i).sourcePoint_bev = [0,-SAD,0];
     stf(i).sourcePoint = stf(i).sourcePoint_bev*rotMat_vectors_T;
-    
-    stf(i).ray.rayPos_bev = [0,0,0];
-    stf(i).ray.targetPoint_bev = [0,SAD,0];
-    stf(i).ray.rayPos = stf(i).ray.rayPos_bev*rotMat_vectors_T;
-    stf(i).ray.targetPoint = stf(i).ray.targetPoint_bev*rotMat_vectors_T;
     
     % automatic energy selection if no energy selected
     if nargin < 3
@@ -74,14 +69,39 @@ for i = 1:length(pln.propStf.gantryAngles)
     end
     
     % generate ray
-    stf(i).ray.energy = machine.data(energyIx).energy;
+    if plane
+        counter = 0;
+        for j = -10:10
+            for k = -10:10
+                counter = counter + 1;
+                stf(i).ray(counter).rayPos_bev = [j*stf(i).bixelWidth,0,k*stf(i).bixelWidth];
+                stf(i).ray(counter).rayPos = stf(i).ray(counter).rayPos_bev*rotMat_vectors_T;
+                
+                stf(i).ray(counter).targetPoint_bev = 2*stf(i).ray(counter).rayPos_bev-stf(i).sourcePoint_bev;
+                stf(i).ray(counter).targetPoint = 2*stf(i).ray(counter).rayPos-stf(i).sourcePoint;
+            end
+        end
+    else
+        stf(i).ray.rayPos_bev = [0,0,0];
+        stf(i).ray.targetPoint_bev = [0,SAD,0];
+        stf(i).ray.rayPos = stf(i).ray.rayPos_bev*rotMat_vectors_T;
+        stf(i).ray.targetPoint = stf(i).ray.targetPoint_bev*rotMat_vectors_T;   
+    end
+    
     if max(machine.data(energyIx).initFocus.SisFWHMAtIso) < currentMinimumFWHM
         currentMinimumFWHM = max(machine.data(energyIx).initFocus.SisFWHMAtIso) - 1;
     end
-    stf(i).ray.focusIx = find(machine.data(energyIx).initFocus.SisFWHMAtIso > currentMinimumFWHM,1,'first');
-    stf(i).ray.rangeShifter = struct;
-    stf(i).ray.rangeShifter.ID = 0;
-    stf(i).ray.rangeShifter.eqThickness = 0;
-    stf(i).ray.rangeShifter.sourceRashiDistance = 0;
+    [stf(i).ray.energy] = deal(machine.data(energyIx).energy);
+    [stf(i).ray.focusIx] = deal(find(machine.data(energyIx).initFocus.SisFWHMAtIso > currentMinimumFWHM,1,'first'));
+    
+    rShifter.ID = 0;
+    rShifter.eqThickness = 0;
+    rShifter.sourceRashiDistance = 0;
+    [stf(i).ray.rangeShifter] = deal(rShifter);
+   
+    stf(i).numOfRays = length(stf(i).ray);
+    stf(i).numOfBixelsPerRay = ones(1,stf(i).numOfRays);
+    stf(i).totalNumOfBixels = stf(i).numOfRays;
 end
+
 end
