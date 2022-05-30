ecd4fd0b05411b9dd8adf91f6ed62eef824c3944
Niklas Wahl <n.wahl@dkfz.de>
2022-05-30 16:48:11 +0200

    implement new interface in matRad_sampling

--- matRad_sampling.m
+++ matRad_sampling.m
@@ -52,13 +52,13 @@ if exist('multScen','var') && ~isempty(multScen)
 else
     % create random scenarios for sampling
     % Deprecated Code
-    pln.multScen = matRad_multScen(ctSamp,'rndScen'); % 'impSamp' or 'wcSamp'
-    pln.multScen.numOfShiftScen = matRad_cfg.defaults.samplingScenarios * ones(3,1);
-    pln.multScen.numOfRangeShiftScen = matRad_cfg.defaults.samplingScenarios;
+    %pln.multScen = matRad_multScen(ctSamp,'rndScen'); % 'impSamp' or 'wcSamp'
+    %pln.multScen.numOfShiftScen = matRad_cfg.defaults.samplingScenarios * ones(3,1);
+    %pln.multScen.numOfRangeShiftScen = matRad_cfg.defaults.samplingScenarios;
     %pln.multScen.totNumScen = matRad_cfg.defaults.samplingScenarios; %This yields an error now
     
-    %pln.multScen = matRad_RandomScenarios(ctSamp);
-    %pln.multScen.nSamples = matRad_cfg.defaults.samplingScenarios;
+    pln.multScen = matRad_RandomScenarios(ctSamp);
+    pln.multScen.nSamples = matRad_cfg.defaults.samplingScenarios;
 end
 
 matRad_cfg.dispInfo('Using %d samples in total \n',pln.multScen.totNumScen);
