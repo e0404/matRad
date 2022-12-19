6d1aa8680552205d7f7f63e4ae4765fe7b49c12e
RemoCristoforetti <recristoforetti@gmail.com>
2022-12-19 16:12:42 +0100

    MixedModality script to generate plns and stf

---
+++ matRad_MixedModality.m
@@ -0,0 +1,94 @@
+matRad_rc;
+matRad_cfg = MatRad_Config.instance();
+
+load TG119.mat
+
+%% 
+% meta information for treatment plan (1) 
+pln(1).numOfFractions  = 5;
+pln(1).radiationMode   = 'protons';           % either photons / protons / helium / carbon
+pln(1).machine         = 'Generic';
+
+% beam geometry settings
+pln(1).propStf.bixelWidth      = 3; % [mm] / also corresponds to lateral spot spacing for particles
+pln(1).propStf.gantryAngles    = [0]; % [?] ;
+pln(1).propStf.couchAngles     = zeros(numel(pln(1).propStf.gantryAngles),1); % [?] ; 
+pln(1).propStf.numOfBeams      = numel(pln(1).propStf.gantryAngles);
+pln(1).propStf.isoCenter       = ones(pln(1).propStf.numOfBeams,1) * matRad_getIsoCenter(cst,ct,0);
+% optimization settings
+pln(1).propDoseCalc.calcLET = 1;
+
+pln(1).propOpt.runDAO          = false;      % 1/true: run DAO, 0/false: don't / will be ignored for particles
+pln(1).propOpt.runSequencing   = false;      % 1/true: run sequencing, 0/false: don't / will be ignored for particles and also triggered by runDAO below
+pln(1).propOpt.spatioTemp      = 0;
+pln(1).propOpt.STScenarios     = 2;
+%pln(1).propOpt.STfractions     = [ 4 4 6 8 8];             % can also do different spread of the fractions between scenes ( make sure sum(STfractions == numOfFractions)
+
+% dose calculation settings
+pln(1).propDoseCalc.doseGrid.resolution.x = 8; % [mm]
+pln(1).propDoseCalc.doseGrid.resolution.y = 8; % [mm]
+pln(1).propDoseCalc.doseGrid.resolution.z = 8; % [mm]
+% pln(1).propDoseCalc.doseGrid.resolution = ct.resolution;
+quantityOpt  = 'RBExD';     % options: physicalDose, effect, RBExD
+%=======================================> Model check error in bioModel
+modelName    = 'MCN';             % none: for photons, protons, carbon            % constRBE: constant RBE for photons and protons 
+                                   % MCN: McNamara-variable RBE model for protons  % WED: Wedenberg-variable RBE model for protons 
+                                   % LEM: Local Effect Model for carbon ions
+
+
+scenGenType  = 'nomScen';          % scenario creation type 'nomScen'  'wcScen' 'impScen' 'rndScen'                                          
+
+% retrieve bio model parameters
+pln(1).bioParam = matRad_bioModel(pln(1).radiationMode,quantityOpt, modelName);
+
+% retrieve scenarios for dose calculation and optimziation
+pln(1).multScen = matRad_multScen(ct,scenGenType);
+
+% meta information for treatment plan (2) 
+pln(2).numOfFractions  = 25;
+pln(2).radiationMode   = 'photons';           % either photons / protons / helium / carbon
+pln(2).machine         = 'Generic';
+
+% beam geometry settings
+pln(2).propStf.bixelWidth      = 5; % [mm] / also corresponds to lateral spot spacing for particles
+pln(2).propStf.gantryAngles    = [0:90:359]; % [?] ;
+pln(2).propStf.couchAngles     = zeros(numel(pln(2).propStf.gantryAngles),1);  % [?] ; 
+pln(2).propStf.numOfBeams      = numel(pln(2).propStf.gantryAngles);
+pln(2).propStf.isoCenter       = ones(pln(2).propStf.numOfBeams,1) * matRad_getIsoCenter(cst,ct,0);
+% optimization settings
+pln(2).propOpt.runDAO          = false;      % 1/true: run DAO, 0/false: don't / will be ignored for particles
+pln(2).propOpt.runSequencing   = false;      % 1/true: run sequencing, 0/false: don't / will be ignored for particles and also triggered by runDAO below
+pln(2).propOpt.spatioTemp      = 0;
+pln(2).propOpt.STScenarios     = 5;
+%pln(2).propOpt.STfractions     = [ 4 4 6 8 8];             % can also do different spread of the fractions between scenes ( make sure sum(STfractions == numOfFractions)
+
+% dose calculation settings
+pln(2).propDoseCalc.doseGrid.resolution.x = 8; % [mm]
+pln(2).propDoseCalc.doseGrid.resolution.y = 8; % [mm]
+pln(2).propDoseCalc.doseGrid.resolution.z = 8; % [mm]
+% pln(2).propDoseCalc.doseGrid.resolution = ct.resolution;
+
+quantityOpt  = 'effect';     % options: physicalDose, effect, RBExD
+modelName    = 'none';             % none: for photons, protons, carbon            % constRBE: constant RBE for photons and protons 
+                                   % MCN: McNamara-variable RBE model for protons  % WED: Wedenberg-variable RBE model for protons 
+                                   % LEM: Local Effect Model for carbon ions
+
+
+scenGenType  = 'nomScen';          % scenario creation type 'nomScen'  'wcScen' 'impScen' 'rndScen'                                          
+
+% retrieve bio model parameters
+pln(2).bioParam = matRad_bioModel(pln(2).radiationMode,quantityOpt, modelName);
+
+% retrieve scenarios for dose calculation and optimziation
+pln(2).multScen = matRad_multScen(ct,scenGenType);
+
+% prepping cst 
+% placing alpha/beta ratios in cst{:,6},
+% different alpha beta ration for each obj of a structure  
+sparecst = 0;
+
+cst = matRad_prepCst(cst, sparecst);
+%% Plan Wrapper
+Pln = matRad_plnWrapper(pln);
+%% Stf Wrapper
+stf = matRad_stfWrapper(ct,cst,Pln);
