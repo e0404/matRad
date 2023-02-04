ec4e8b23ff048be9005ec93f6d24fa2e40b8d0e2
Niklas Wahl <niklaswahl2011@gmail.com>
2023-02-04 15:10:49 +0100

    update scripts for github folder

--- .github/before_install_linux.sh
+++ .github/before_install_linux.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-sudo chmod +x .travis/runtests.sh
+sudo chmod +x .github/runtests.sh
 sudo chmod +x MCsquare/bin/MCsquare_linux
 
 mv optimization/optimizer/ipopt.m optimization/optimizer/ipopt.m.bak
