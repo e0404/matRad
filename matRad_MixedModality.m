d55da4deb6401245576464cbf9c799ad09e2cf11
RemoCristoforetti <recristoforetti@gmail.com>
2023-07-26 15:45:33 +0200

    Add simple visulization to mixModality script

--- matRad_MixedModality.m
+++ matRad_MixedModality.m
@@ -3,8 +3,8 @@ matRad_cfg = MatRad_Config.instance();
 matRad_cfg.propOpt.defaultMaxIter = 50000;
 load 'TG119.mat'
 
-% 
-% meta information for treatment plan (1) 
+
+%% meta information for treatment plan (1) 
 pln(1).numOfFractions  = 5;
 pln(1).radiationMode   = 'protons';           % either photons / protons / helium / carbon
 pln(1).machine         = 'Generic';
@@ -21,17 +21,18 @@ pln(1).propDoseCalc.calcLET = 1;
 pln(1).propOpt.runDAO          = false;      % 1/true: run DAO, 0/false: don't / will be ignored for particles
 pln(1).propOpt.runSequencing   = false;      % 1/true: run sequencing, 0/false: don't / will be ignored for particles and also triggered by runDAO below
 pln(1).propOpt.spatioTemp      = 0;
-pln(1).propOpt.STscenarios     = 2;
+%pln(1).propOpt.STscenarios     = 2;
 %pln(1).propOpt.STfractions     = [ 4 4 6 8 8];             % can also do different spread of the fractions between scenes ( make sure sum(STfractions == numOfFractions)
 
 % dose calculation settings
 pln(1).propDoseCalc.doseGrid.resolution.x = 8; % [mm]
 pln(1).propDoseCalc.doseGrid.resolution.y = 8; % [mm]
 pln(1).propDoseCalc.doseGrid.resolution.z = 8; % [mm]
+
 % pln(1).propDoseCalc.doseGrid.resolution = ct.resolution;
-quantityOpt  = 'physicalDose';     % options: physicalDose, effect, RBExD
+quantityOpt  = 'effect';     % options: physicalDose, effect, RBExD
 %=======================================> Model check error in bioModel
-modelName    = 'none';             % none: for photons, protons, carbon            % constRBE: constant RBE for photons and protons 
+modelName    = 'MCN';             % none: for photons, protons, carbon            % constRBE: constant RBE for photons and protons 
                                    % MCN: McNamara-variable RBE model for protons  % WED: Wedenberg-variable RBE model for protons 
                                    % LEM: Local Effect Model for carbon ions
 
@@ -51,7 +52,7 @@ pln(2).machine         = 'Generic';
 
 % beam geometry settings
 pln(2).propStf.bixelWidth      = 5; % [mm] / also corresponds to lateral spot spacing for particles
-pln(2).propStf.gantryAngles    = [0:90:359]; % [?] ;
+pln(2).propStf.gantryAngles    = [0:80:359]; % [?] ;
 pln(2).propStf.couchAngles     = zeros(numel(pln(2).propStf.gantryAngles),1);  % [?] ; 
 pln(2).propStf.numOfBeams      = numel(pln(2).propStf.gantryAngles);
 pln(2).propStf.isoCenter       = ones(pln(2).propStf.numOfBeams,1) * matRad_getIsoCenter(cst,ct,0);
@@ -59,7 +60,7 @@ pln(2).propStf.isoCenter       = ones(pln(2).propStf.numOfBeams,1) * matRad_getI
 pln(2).propOpt.runDAO          = false;      % 1/true: run DAO, 0/false: don't / will be ignored for particles
 pln(2).propOpt.runSequencing   = false;      % 1/true: run sequencing, 0/false: don't / will be ignored for particles and also triggered by runDAO below
 pln(2).propOpt.spatioTemp      = 0;
-pln(2).propOpt.STscenarios     = 5;
+%pln(2).propOpt.STscenarios     = 5;
 %pln(2).propOpt.STfractions     = [ 4 4 6 8 8];             % can also do different spread of the fractions between scenes ( make sure sum(STfractions == numOfFractions)
 
 % dose calculation settings
@@ -68,7 +69,7 @@ pln(2).propDoseCalc.doseGrid.resolution.y = 8; % [mm]
 pln(2).propDoseCalc.doseGrid.resolution.z = 8; % [mm]
 % pln(2).propDoseCalc.doseGrid.resolution = ct.resolution;
 
-quantityOpt  = 'physicalDose';     % options: physicalDose, effect, RBExD
+quantityOpt  = 'effect';     % options: physicalDose, effect, RBExD
 modelName    = 'none';             % none: for photons, protons, carbon            % constRBE: constant RBE for photons and protons 
                                    % MCN: McNamara-variable RBE model for protons  % WED: Wedenberg-variable RBE model for protons 
                                    % LEM: Local Effect Model for carbon ions
@@ -88,11 +89,74 @@ pln(2).multScen = matRad_multScen(ct,scenGenType);
 sparecst = 0;
 
 cst = matRad_prepCst(cst, sparecst);
-% Plan Wrapper
+%% Plan Wrapper
 plnJO = matRad_plnWrapper(pln);
-% Stf Wrapper
+%% Stf Wrapper
 stf = matRad_stfWrapper(ct,cst,plnJO);
-% Dij Calculation
+
+%% Dij Calculation
 dij = matRad_calcCombiDose(ct,stf,plnJO,cst,false);
-% Fluence optimization 
-resultGUI = matRad_fluenceOptimizationJO(dij,cst,plnJO)
+%% Fluence optimization
+resultGUI = matRad_fluenceOptimizationJO(dij,cst,plnJO);
+
+%% Visualization
+slice = 59;
+qtOpt = plnJO.bioParam.quantityOpt;
+modalities = {'proton', 'photon'};
+plan_JO = pln(1).numOfFractions * resultGUI{1}.(qtOpt) + pln(2).numOfFractions * resultGUI{2}.(qtOpt);
+
+if any(plnJO.propOpt.spatioTemp)
+    f = figure;
+    protonScenarios = plnJO.propOpt.STscenarios(1);
+    if protonScenarios>1
+        for scenarioIdx = 1:protonScenarios
+            subplot(3,max([plnJO.propOpt.STscenarios])+1,scenarioIdx);
+            imagesc(resultGUI{1}.([qtOpt, '_STscenario_', num2str(scenarioIdx)])(:,:,slice));
+            matRad_plotVoiContourSlice(gca(f), cst,ct.cube, 1, 1,3,slice);
+            title(['proton plan scenario ', num2str(scenarioIdx)]);
+            colorbar();
+        end
+    end
+    subplot(3,max([plnJO.propOpt.STscenarios])+1,max([plnJO.propOpt.STscenarios])+1);
+    imagesc(resultGUI{1}.(qtOpt)(:,:,slice));
+    matRad_plotVoiContourSlice(gca(f), cst,ct.cube, 1, 1,3,slice);
+    title('total proton plan');
+    colorbar();
+   
+    photonScenarios = plnJO.propOpt.STscenarios(2);
+    if photonScenarios>1
+        for scenarioIdx = 1:photonScenarios
+            subplot(3,max([plnJO.propOpt.STscenarios])+1,max([plnJO.propOpt.STscenarios])+1 + scenarioIdx);
+            imagesc(resultGUI{2}.([qtOpt, '_STscenario_', num2str(scenarioIdx)])(:,:,slice));
+            matRad_plotVoiContourSlice(gca(f), cst,ct.cube, 1, 1,3,slice);
+            title(['photon plan scenario ', num2str(scenarioIdx)]);
+            colorbar();
+        end
+    end
+    subplot(3,max([plnJO.propOpt.STscenarios])+1,2*(max([plnJO.propOpt.STscenarios])+1));
+    imagesc(resultGUI{2}.(qtOpt)(:,:,slice));
+    matRad_plotVoiContourSlice(gca(f), cst,ct.cube, 1, 1,3,slice);
+    title('total photon plan');
+    colorbar();
+    subplot(3,max([plnJO.propOpt.STscenarios])+1,3*(max([plnJO.propOpt.STscenarios])+1));
+    imagesc(plan_JO(:,:,slice));
+    matRad_plotVoiContourSlice(gca(f), cst,ct.cube, 1, 1,3,slice);
+    title('total plan');
+    colorbar();
+else
+%%% Plot photon-proton single scenarios total plan
+    f = figure;
+
+    for modalityIdx =1:plnJO.numOfModalities
+        subplot(1,3,modalityIdx);
+        imagesc(resultGUI{modalityIdx}.(qtOpt)(:,:,slice));
+        matRad_plotVoiContourSlice(gca(f), cst,ct.cube, 1, 1,3,slice);
+        title([ modalities{modalityIdx}, ' plan']);
+    end
+
+    subplot(1,3,3);
+    imagesc(plan_JO(:,:,slice));
+    matRad_plotVoiContourSlice(gca(f), cst,ct.cube, 1, 1,3,slice);
+
+    title('Total plan');
+end
