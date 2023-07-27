5f7780fd27342125958c47dc8f1b3473b4348fe0
RemoCristoforetti <recristoforetti@gmail.com>
2023-07-27 10:55:49 +0200

    script clean up, sections and visualization

--- matRad_MixedModality.m
+++ matRad_MixedModality.m
@@ -3,8 +3,8 @@ matRad_cfg = MatRad_Config.instance();
 matRad_cfg.propOpt.defaultMaxIter = 50000;
 load 'TG119.mat'
 
-
-%% meta information for treatment plan (1) 
+% 
+% meta information for treatment plan (1) 
 pln(1).numOfFractions  = 5;
 pln(1).radiationMode   = 'protons';           % either photons / protons / helium / carbon
 pln(1).machine         = 'Generic';
@@ -21,18 +21,17 @@ pln(1).propDoseCalc.calcLET = 1;
 pln(1).propOpt.runDAO          = false;      % 1/true: run DAO, 0/false: don't / will be ignored for particles
 pln(1).propOpt.runSequencing   = false;      % 1/true: run sequencing, 0/false: don't / will be ignored for particles and also triggered by runDAO below
 pln(1).propOpt.spatioTemp      = 0;
-%pln(1).propOpt.STscenarios     = 2;
+pln(1).propOpt.STscenarios     = 2;
 %pln(1).propOpt.STfractions     = [ 4 4 6 8 8];             % can also do different spread of the fractions between scenes ( make sure sum(STfractions == numOfFractions)
 
 % dose calculation settings
 pln(1).propDoseCalc.doseGrid.resolution.x = 8; % [mm]
 pln(1).propDoseCalc.doseGrid.resolution.y = 8; % [mm]
 pln(1).propDoseCalc.doseGrid.resolution.z = 8; % [mm]
-
 % pln(1).propDoseCalc.doseGrid.resolution = ct.resolution;
-quantityOpt  = 'effect';     % options: physicalDose, effect, RBExD
+quantityOpt  = 'physicalDose';     % options: physicalDose, effect, RBExD
 %=======================================> Model check error in bioModel
-modelName    = 'MCN';             % none: for photons, protons, carbon            % constRBE: constant RBE for photons and protons 
+modelName    = 'none';             % none: for photons, protons, carbon            % constRBE: constant RBE for photons and protons 
                                    % MCN: McNamara-variable RBE model for protons  % WED: Wedenberg-variable RBE model for protons 
                                    % LEM: Local Effect Model for carbon ions
 
@@ -52,7 +51,7 @@ pln(2).machine         = 'Generic';
 
 % beam geometry settings
 pln(2).propStf.bixelWidth      = 5; % [mm] / also corresponds to lateral spot spacing for particles
-pln(2).propStf.gantryAngles    = [0:80:359]; % [?] ;
+pln(2).propStf.gantryAngles    = [0:90:359]; % [?] ;
 pln(2).propStf.couchAngles     = zeros(numel(pln(2).propStf.gantryAngles),1);  % [?] ; 
 pln(2).propStf.numOfBeams      = numel(pln(2).propStf.gantryAngles);
 pln(2).propStf.isoCenter       = ones(pln(2).propStf.numOfBeams,1) * matRad_getIsoCenter(cst,ct,0);
@@ -60,7 +59,7 @@ pln(2).propStf.isoCenter       = ones(pln(2).propStf.numOfBeams,1) * matRad_getI
 pln(2).propOpt.runDAO          = false;      % 1/true: run DAO, 0/false: don't / will be ignored for particles
 pln(2).propOpt.runSequencing   = false;      % 1/true: run sequencing, 0/false: don't / will be ignored for particles and also triggered by runDAO below
 pln(2).propOpt.spatioTemp      = 0;
-%pln(2).propOpt.STscenarios     = 5;
+pln(2).propOpt.STscenarios     = 5;
 %pln(2).propOpt.STfractions     = [ 4 4 6 8 8];             % can also do different spread of the fractions between scenes ( make sure sum(STfractions == numOfFractions)
 
 % dose calculation settings
@@ -69,7 +68,7 @@ pln(2).propDoseCalc.doseGrid.resolution.y = 8; % [mm]
 pln(2).propDoseCalc.doseGrid.resolution.z = 8; % [mm]
 % pln(2).propDoseCalc.doseGrid.resolution = ct.resolution;
 
-quantityOpt  = 'effect';     % options: physicalDose, effect, RBExD
+quantityOpt  = 'physicalDose';     % options: physicalDose, effect, RBExD
 modelName    = 'none';             % none: for photons, protons, carbon            % constRBE: constant RBE for photons and protons 
                                    % MCN: McNamara-variable RBE model for protons  % WED: Wedenberg-variable RBE model for protons 
                                    % LEM: Local Effect Model for carbon ions
@@ -93,12 +92,10 @@ cst = matRad_prepCst(cst, sparecst);
 plnJO = matRad_plnWrapper(pln);
 %% Stf Wrapper
 stf = matRad_stfWrapper(ct,cst,plnJO);
-
 %% Dij Calculation
 dij = matRad_calcCombiDose(ct,stf,plnJO,cst,false);
-%% Fluence optimization
+%% Fluence optimization 
 resultGUI = matRad_fluenceOptimizationJO(dij,cst,plnJO);
-
 %% Visualization
 slice = 59;
 qtOpt = plnJO.bioParam.quantityOpt;
