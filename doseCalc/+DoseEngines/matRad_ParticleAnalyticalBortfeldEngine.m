10f209d0768363c9b99e0df3a0bdd158d11aca7a
Niklas Wahl <n.wahl@dkfz.de>
2023-11-14 18:58:02 +0100

    Fix new Analytical Bortfeld Engine

--- doseCalc/+DoseEngines/matRad_ParticleAnalyticalBortfeldEngine.m
+++ doseCalc/+DoseEngines/matRad_ParticleAnalyticalBortfeldEngine.m
@@ -25,38 +25,29 @@ classdef matRad_ParticleAnalyticalBortfeldEngine < DoseEngines.matRad_ParticlePe
     end
 
     properties (SetAccess = public, GetAccess = public)
-
-        %calcBortfeldBragg = true;
-        %modeParabCyl = true;           % Boolean which defines a handmade, slower (0) and matLab, faster (1) parabolic cylinder function
-
-        %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-        % Constants of the model
-        % PHYSICAL CONSTANTS
-
-
-        % PARAMETERS OF THE TARGET MATERIAL (WAter)
+        
+        % target material parameters (Water)
         massDensity = 0.997;                            % Mass Density of the medium in (g/cm^3)
         p           = 1.77;                             % Exponent in the Bragg-Kleemann rule
         alpha       = 2.2*10^(-3);                      % Material-dependent constant in the Bragg-Kleemann rule
         beta        = 0.012;                            % Slope parameter of the linear fluence reduction
         gammaNuc    = 0.6;                              % Fraction of locally absorbed energy in nuclear interactions
-
         Z           = 10;                               % N of electrons per molecule (water)
         MM          = 18.01;                            % Molar mass in g/mol (water)
 
-        % PARAMETERS OF THE BEAM
-        phi0         = 1;                                % Primary proton fluence
-        epsilonTail  = 0.1;                              % (Small) fraction of primary fluence \phi_0 contributing to the linear "tail" of the energy spectrum
-        sigmaEnergy  = 0.01;                             % sigma of the gaussian energy spectrum
+        % Beam parameters
+        phi0         = 1;                               % Primary proton fluence
+        epsilonTail  = 0.1;                             % (Small) fraction of primary fluence \phi_0 contributing to the linear "tail" of the energy spectrum
+        sigmaEnergy  = 0.01;                            % sigma of the gaussian energy spectrum
 
-        radLenght    = 36.3;
-        modeWidth   = true;               % Boolean which defines a monoenergetic (0) and gaussian (1) energy spectrum
+        radLength    = 36.3;
+        modeWidth   = true;                             % Boolean which defines a monoenergetic (0) and gaussian (1) energy spectrum
     end
 
-    properties (Constant)
-        epsilon0         = 8.854*10^(-12);                   % Vacuum dielectric constant in (C^2/(N*m^2))
-        electronCharge   = 1.602*10^(-19);                   % Electron charge in (C)
-        avogadroNum      = 6.022*10^23;                      % Avogadro number
+    properties (Access = private, Constant)
+        epsilon0         = 8.854*10^(-12);              % Vacuum dielectric constant in (C^2/(N*m^2))
+        electronCharge   = 1.602*10^(-19);              % Electron charge in (C)
+        avogadroNum      = 6.022*10^23;                 % Avogadro number
     end
 
     methods
@@ -90,6 +81,33 @@ classdef matRad_ParticleAnalyticalBortfeldEngine < DoseEngines.matRad_ParticlePe
             end
             
             [dij,ct,cst,stf] = this.calcDoseInit@DoseEngines.matRad_ParticlePencilBeamEngineAbstract(ct,cst,stf);
+        end
+
+        function [currBixel] = getBixelIndicesOnRay(this,currBixel,currRay)
+            
+            % create offset vector to account for additional offsets modelled in the base data and a potential
+            % range shifter. In the following, we only perform dose calculation for voxels having a radiological depth
+            % that is within the limits of the base data set (-> machine.data(i).dephts). By this means, we only allow
+            % interpolations in this.calcParticleDoseBixel() and avoid extrapolations.
+            %urrBixel.offsetRadDepth = currBixel.baseData.offset + currBixel.radDepthOffset;
+            tmpOffset = currBixel.baseData.offset - currBixel.radDepthOffset;
+            
+            maxDepth = 1.15 * currBixel.baseData.range;
+
+            % find depth depended lateral cut off
+            if this.dosimetricLateralCutOff == 1
+                currIx = currRay.radDepths <= maxDepth + tmpOffset;
+            elseif this.dosimetricLateralCutOff < 1 && this.dosimetricLateralCutOff > 0
+                currIx = currRay.radDepths <= maxDepth + tmpOffset;
+                sigmaSq = this.calcSigmaLatMCS(currRay.radDepths(currIx) - tmpOffset, currBixel.baseData.energy).^2 + currBixel.sigmaIniSq;
+                currIx(currIx) = currRay.radialDist_sq(currIx) < currBixel.baseData.LatCutOff.numSig.^2*sigmaSq;
+            else
+                matRad_cfg = MatRad_Config.instance();
+                matRad_cfg.dispError('Cutoff must be a value between 0 and 1!')
+            end
+
+            currBixel.subRayIx = currIx;
+            currBixel.ix = currRay.ix(currIx);
         end
 
         function X = interpolateKernelsInDepth(this,bixel)
@@ -264,7 +282,7 @@ classdef matRad_ParticleAnalyticalBortfeldEngine < DoseEngines.matRad_ParticlePe
 
             range = this.alpha*primaryEnergy.^this.p;        % Range-Energy relation, i.e. Bragg-Kleemann rule
 
-            sigma1      = @(z)      14.1^2 /this.radLenght * (1+1/9*log10(z./this.radLenght)).^2;
+            sigma1      = @(z)      14.1^2 /this.radLength * (1+1/9*log10(z./this.radLength)).^2;
             sigma21     = @(z)   1  ./(1-2/this.p)  .*( range.^(1-2/this.p).*(range-z).^2 - (range-z).^(3-2/this.p) );
             sigma22     = @(z)   -2*(range-z)  ./(2-2/this.p)  .*( range.^(2-2/this.p) - (range-z).^(2-2/this.p) );
             sigma23     = @(z)   1   ./(3-2/this.p)  .*( range.^(3-2/this.p) - (range-z).^(3-2/this.p) );
@@ -285,74 +303,13 @@ classdef matRad_ParticleAnalyticalBortfeldEngine < DoseEngines.matRad_ParticlePe
         function calcLateralParticleCutOff(this,cutOffLevel,~)
             for i = 1:numel(this.machine.data)
                 this.machine.data(i).LatCutOff.CompFac = 1-cutOffLevel;
-                this.machine.data(i).LatCutOff.numSig  = sqrt(2) * erfinv(cutOffLevel);
-            end
-        end
-
-        function [currBixel] = getBixelIndicesOnRay(this,currBixel,currRay)
-            
-            % create offset vector to account for additional offsets modelled in the base data and a potential
-            % range shifter. In the following, we only perform dose calculation for voxels having a radiological depth
-            % that is within the limits of the base data set (-> machine.data(i).dephts). By this means, we only allow
-            % interpolations in this.calcParticleDoseBixel() and avoid extrapolations.
-            %urrBixel.offsetRadDepth = currBixel.baseData.offset + currBixel.radDepthOffset;
-            tmpOffset = currBixel.baseData.offset - currBixel.radDepthOffset;
-
-            % find depth depended lateral cut off
-            if this.dosimetricLateralCutOff == 1
-                currIx = currRay.radDepths <= currBixel.baseData.depths(end) + currBixel.offsetRadDepth;
-            elseif this.dosimetricLateralCutOff < 1 && this.dosimetricLateralCutOff > 0
-
-                if length(currBixel.baseData.LatCutOff.CutOff) > 1
-                    currIx = matRad_interp1((currBixel.baseData.LatCutOff.depths + tmpOffset)',(currBixel.baseData.LatCutOff.CutOff.^2)', currRay.radDepths,'nearest') >= currRay.radialDist_sq;
+                this.machine.data(i).LatCutOff.numSig  = sqrt(2) * sqrt(gammaincinv(0.995,1)); %For a 2D symmetric gaussian we need the inverse of the incomplete Gamma function for defining the CutOff
+                this.machine.data(i).LatCutOff.maxSigmaIni = max([this.machine.data(i).initFocus(:).SisFWHMAtIso]) ./ 2.3548;
+                if ~isfield(this.machine.data(i),'range')
+                    this.machine.data(i).range = 10 * this.alpha*this.machine.data(i).energy.^this.p;
                 end
-            else
-                matRad_cfg = MatRad_Config.instance();
-                matRad_cfg.dispError('Cutoff must be a value between 0 and 1!')
+                this.machine.data(i).LatCutOff.CutOff = this.machine.data(i).LatCutOff.numSig * sqrt(this.machine.data(i).LatCutOff.maxSigmaIni^2 + this.calcSigmaLatMCS(this.machine.data(i).range,this.machine.data(i).energy)^2);
             end
-
-            currBixel.subRayIx = currIx;
-            currBixel.ix = currRay.ix(currIx);
-        end
-
-        function [currBixel] = getBixelIndicesOnRay(this,currBixel,currRay)
-            
-            % create offset vector to account for additional offsets modelled in the base data and a potential
-            % range shifter. In the following, we only perform dose calculation for voxels having a radiological depth
-            % that is within the limits of the base data set (-> machine.data(i).dephts). By this means, we only allow
-            % interpolations in this.calcParticleDoseBixel() and avoid extrapolations.
-            %urrBixel.offsetRadDepth = currBixel.baseData.offset + currBixel.radDepthOffset;
-            tmpOffset = currBixel.baseData.offset - currBixel.radDepthOffset;
-
-            % find depth depended lateral cut off
-            if this.dosimetricLateralCutOff == 1
-                currIx = currRay.radDepths <= currBixel.baseData.depths(end) + currBixel.offsetRadDepth;
-            elseif this.dosimetricLateralCutOff < 1 && this.dosimetricLateralCutOff > 0
-                
-                %{
-                % Old two step clipping. Seems to be slower than doing it
-                at once using nearest neighbor extrapolation
-                % perform rough 2D clipping
-                currIx = currRay.radDepths <= currBixel.baseData.depths(end) + tmpOffset & ...
-                    currRay.radialDist_sq <= max(currBixel.baseData.LatCutOff.CutOff.^2);
-
-                % peform fine 2D clipping
-                if length(currBixel.baseData.LatCutOff.CutOff) > 1
-                    currIx(currIx) = matRad_interp1((currBixel.baseData.LatCutOff.depths + tmpOffset)',...
-                        (currBixel.baseData.LatCutOff.CutOff.^2)', currRay.radDepths(currIx)) >= currRay.radialDist_sq(currIx);
-                end
-                %}                
-
-                if length(currBixel.baseData.LatCutOff.CutOff) > 1
-                    currIx = matRad_interp1((currBixel.baseData.LatCutOff.depths + tmpOffset)',(currBixel.baseData.LatCutOff.CutOff.^2)', currRay.radDepths,'nearest') >= currRay.radialDist_sq;
-                end
-            else
-                matRad_cfg = MatRad_Config.instance();
-                matRad_cfg.dispError('Cutoff must be a value between 0 and 1!')
-            end
-
-            currBixel.subRayIx = currIx;
-            currBixel.ix = currRay.ix(currIx);
         end
     end
 
