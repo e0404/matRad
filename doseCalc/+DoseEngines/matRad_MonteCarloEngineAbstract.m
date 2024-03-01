ad31fbc44037275f730b3645b82215cf37b349b1
JenHardt <jennifer.hardt@dkfz-heidelberg.de>
2024-03-01 16:40:07 +0100

    pln is no longer a variable, in the writeStfField adjust for that

--- doseCalc/+DoseEngines/matRad_MonteCarloEngineAbstract.m
+++ doseCalc/+DoseEngines/matRad_MonteCarloEngineAbstract.m
@@ -61,7 +61,7 @@ classdef (Abstract) matRad_MonteCarloEngineAbstract < DoseEngines.matRad_DoseEng
 
             % copy bixel weight vector into stf struct
             if nargin == 5
-                if sum([stf.totalNumOfBixels]) ~= numel(w)
+                if sum([stf.totalNumOfBixels]) ~= numel(w) && ~isfield([stf.ray],'shapes')
                     matRad_cfg.dispError('weighting does not match steering information')
                 end
                 counter = 0;
@@ -90,14 +90,15 @@ classdef (Abstract) matRad_MonteCarloEngineAbstract < DoseEngines.matRad_DoseEng
             this.calcDoseDirect = true;
             dij = this.calcDose(ct,cst,stf);
 
-            % hack dij struct
-            dij.numOfBeams = 1;
-            dij.beamNum = 1;
-
-            % calculate cubes; use uniform weights here, weighting with actual fluence
-            % already performed in dij construction
-            resultGUI    = matRad_calcCubes(sum(w),dij);
-
+            if ~this.externalCalculation
+                % hack dij struct
+                dij.numOfBeams = 1;
+                dij.beamNum = 1;
+    
+                % calculate cubes; use uniform weights here, weighting with actual fluence
+                % already performed in dij construction
+                resultGUI    = matRad_calcCubes(sum(w),dij);
+            end
             % remember original fluence weights
             resultGUI.w  = w;
         end
