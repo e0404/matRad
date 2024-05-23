64ed7dbd3e5d009a07fdfa11a55cdcc6e8cc8008
mdallas <mariosdallas2995@gmail.com>
2024-05-23 14:46:05 +0200

    123

--- test/brachy/test_calcBrachyDoseTest.m
+++ test/brachy/test_calcBrachyDoseTest.m
@@ -6,5 +6,19 @@ initTestSuite;
 
 
 
-
+% test if dij struct has required shape
+function rightOutput(testCase)
+load PROSTATE.mat ct cst;
+load examplePlnAndStf.mat pln stf;
+pln.propDoseCalc.durationImplanted = Inf;
+            
+       
+dij = testCase.engine.setCalcDose(ct,cst,stf);
+testCase.verifyTrue(isfield(dij,'doseGrid'));
+testCase.verifyTrue(isfield(dij,'physicalDose'));
+% testCase.verifyTrue(isfield(dij,'basedata'));
+testCase.verifyTrue(iscell(dij.physicalDose));
+            
+clear ct cst pln;
+end
 
