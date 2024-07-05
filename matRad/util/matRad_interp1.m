c4d32ff5a7bd73979e62164119c9291e21ed3e11
Niklas Wahl <n.wahl@dkfz.de>
2024-07-05 13:51:42 +0200

    matRad interpolation wrapper fix for non-vector input

--- matRad/util/matRad_interp1.m
+++ matRad/util/matRad_interp1.m
@@ -1,5 +1,5 @@
 function y = matRad_interp1(xi,yi,x,extrapolation)
-% interpolates 1-D data (table lookup) and utilizes griddedInterpolant 
+% interpolates 1-D data (table lookup) and utilizes griddedInterpolant
 % if availabe in the used MATLAB version
 %
 % call
@@ -7,17 +7,17 @@ function y = matRad_interp1(xi,yi,x,extrapolation)
 %   y = matRad_interp1(xi,yi,x,extrapolation)
 %
 % input
-%   xi:             sample points 
+%   xi:             sample points
 %   yi:             corresponding data to sample points
 %   x:              query points for interpolation
-%   extrapolation:  (optional) strategy for extrapolation. Similar to 
+%   extrapolation:  (optional) strategy for extrapolation. Similar to
 %                   interp1. NaN is the default extrapolation value
-%	
+%
 % output
-%   y:              interpolated data   
+%   y:              interpolated data
 %
 % Note that all input data has to be given as column vectors for a correct
-% interpolation. yi can be a matrix consisting out of several 1-D datasets 
+% interpolation. yi can be a matrix consisting out of several 1-D datasets
 % in each column, which will all be interpolated for the given query points.
 %
 % Reference
@@ -25,13 +25,13 @@ function y = matRad_interp1(xi,yi,x,extrapolation)
 %
 % %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %
-% Copyright 2020 the matRad development team. 
-% 
-% This file is part of the matRad project. It is subject to the license 
-% terms in the LICENSE file found in the top-level directory of this 
-% distribution and at https://github.com/e0404/matRad/LICENSES.txt. No part 
-% of the matRad project, including this file, may be copied, modified, 
-% propagated, or distributed except according to the terms contained in the 
+% Copyright 2020 the matRad development team.
+%
+% This file is part of the matRad project. It is subject to the license
+% terms in the LICENSE file found in the top-level directory of this
+% distribution and at https://github.com/e0404/matRad/LICENSES.txt. No part
+% of the matRad project, including this file, may be copied, modified,
+% propagated, or distributed except according to the terms contained in the
 % LICENSE file.
 %
 % %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -56,14 +56,14 @@ end
 
 % manual interpolation for only one query point to save time
 if numel(x) == 1
-    
+
     ix1 = find((x >= xi), 1, 'last');
     ix2 = find((x <= xi), 1, 'first');
-    
+
     if ix1 == ix2
         y = yi(ix1,:);
     elseif ix2 == ix1 + 1
-        y = yi(ix1,:) + ( yi(ix2,:)-yi(ix1,:) ) * ( x - xi(ix1) ) / ( xi(ix2) - xi(ix1) );    
+        y = yi(ix1,:) + ( yi(ix2,:)-yi(ix1,:) ) * ( x - xi(ix1) ) / ( xi(ix2) - xi(ix1) );
     else
         if isscalar(extrapolation)
             y = extrapolation;
@@ -71,29 +71,29 @@ if numel(x) == 1
             %In this unlikely event fall back to classic
             y = interp1(xi,yi,x,'linear',extrapolation);
         elseif strcmp(extrapolation,'nearest')
-            if x < min(xi) 
+            if x < min(xi)
                 y = min(yi);
             elseif x > max(xi)
                 y = max(yi);
             else
-                y = yi(ix1,:) + ( yi(ix2,:)-yi(ix1,:) ) * ( x - xi(ix1) ) / ( xi(ix2) - xi(ix1) );    
+                y = yi(ix1,:) + ( yi(ix2,:)-yi(ix1,:) ) * ( x - xi(ix1) ) / ( xi(ix2) - xi(ix1) );
             end
         else
             matRad_cfg = MatRad_Config.instance();
             matRad_cfg.dispError('Invalid extrapolation argument ''%s''!',extrapolation);
-        end 
+        end
     end
-        
+
 elseif isGriddedInterpolantAvailable
-    
+
     if isscalar(extrapolation)
         extrapmethod = 'none';
     elseif strcmp(extrapolation,'extrap')
         extrapmethod = 'linear';
-    else 
+    else
         extrapmethod = extrapolation;
     end
-    
+
     if size(yi,2) > 1
 		% interpolation for multiple 1-D datasets
         samplePoints = {xi, 1:size(yi,2)};
@@ -103,16 +103,16 @@ elseif isGriddedInterpolantAvailable
         samplePoints = {xi};
         queryPoints  = {x};
     end
-    
+
     F = griddedInterpolant(samplePoints,yi,'linear',extrapmethod);
 
     y = F(queryPoints);
-    
+
     if isnumeric(extrapolation) && ~isnan(extrapolation)
         y(isnan(y)) = extrapolation;
     end
-        
-        
+
+
 else
     % for older matlab versions or octave use this code
     if isscalar(extrapolation) || strcmp(extrapolation,'extrap')
@@ -127,11 +127,11 @@ else
             extrapmethod = 'extrap';
         end
     end
-    y = interp1(xi,yi,x,'linear',extrapmethod);
+    y = interp1(xi,yi,x(:),'linear',extrapmethod);
 
     %Manual nearest neighbor assignment for extrapolation
     if strcmp(extrapolation,'nearest')
-        y(x < min(xi),:) = repmat(min(yi),sum(x<min(xi)),1);
-        y(x > max(xi),:) = repmat(max(yi),sum(x>max(xi)),1);
+        y(x(:) < min(xi),:) = repmat(min(yi),sum(x(:)<min(xi)),1);
+        y(x(:) > max(xi),:) = repmat(max(yi),sum(x(:)>max(xi)),1);
     end
 end
