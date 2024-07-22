52940e6b64a4d73e8068a764a5930d871d7ad0bf
Niklas Wahl <n.wahl@dkfz.de>
2024-07-22 11:50:57 +0200

    iterate fix version

--- matRad/util/matRad_version.m
+++ matRad/util/matRad_version.m
@@ -33,7 +33,7 @@ function [versionString,matRadVer] = matRad_version(matRadRoot)
 matRadVer.name = 'Cleve';
 matRadVer.major = 3;
 matRadVer.minor = 0;
-matRadVer.patch = 5;
+matRadVer.patch = 6;
 
 tagged = false;
 
