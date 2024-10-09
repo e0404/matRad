f89d141e9887e3d2ee50122656ff999df291e3bb
Niklas Wahl <n.wahl@dkfz.de>
2024-10-09 11:23:15 +0200

    fix in DICOM exporter to pass tests

--- matRad/dicom/@matRad_DicomExporter/matRad_exportDicomCt.m
+++ matRad/dicom/@matRad_DicomExporter/matRad_exportDicomCt.m
@@ -69,19 +69,13 @@ ct = obj.ct;
 nSlices = ct.cubeDim(3);
 %Create X Y Z vectors if not present
 if ~any(isfield(ct,{'x','y','z'}))
-    %positionOffset = transpose(ct.cubeDim ./ 2);
-    %positionOffset = ct.cubeDim .* [ct.resolution.y, ct.resolution.x, ct.resolution.z] ./ 2;
-%     positionOffset = [ct.resolution.y, ct.resolution.x, ct.resolution.z] ./ 2;
-%     ct.x = ct.resolution.x*[0:ct.cubeDim(2)-1] - positionOffset(2);
-%     ct.y = ct.resolution.y*[0:ct.cubeDim(1)-1] - positionOffset(1);
-%     ct.z = ct.resolution.z*[0:ct.cubeDim(3)-1] - positionOffset(3);
     ct = matRad_getWorldAxes(ct);
 end
 
 obj.ct = ct;
 
 %Since we are exporting HU directly --> no rescaling in any case
-meta.SliceThickness = num2str(ct.resolution.z);
+meta.SliceThickness = ct.resolution.z;
 meta.PixelSpacing   = [ct.resolution.y; ct.resolution.x];
 meta.ImageOrientationPatient = [1;0;0;0;1;0]; %lps
 meta.RescaleType = 'HU';
