57c311dd8027c4b6bcf3efad1b1934552f338730
LucasSommer <40032929+LucasSommer@users.noreply.github.com>
2025-02-21 14:50:47 +0100

    2025_02_matRadHackathon_#4
    Neutrons added to bio model.

--- matRad/bioModels/matRad_EmptyBiologicalModel.m
+++ matRad/bioModels/matRad_EmptyBiologicalModel.m
@@ -16,7 +16,7 @@ classdef matRad_EmptyBiologicalModel < matRad_BiologicalModel
 % %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
     properties (Constant)
         model = 'none';
-        possibleRadiationModes =  {'photons', 'protons', 'carbon', 'helium', 'brachy'};
+        possibleRadiationModes =  {'photons', 'protons', 'carbon', 'helium', 'brachy', 'neutrons'};
         requiredQuantities = {};
         defaultReportQuantity = 'physicalDose';
     end
