c67b2a9ef7a791c761ecb2d050fbbcd9648f6310
Niklas Wahl <n.wahl@dkfz-heidelberg.de>
2025-08-12 11:08:52 +0900

    we need to allow more index computations in fine sampling engine

--- matRad/doseCalc/+DoseEngines/matRad_ParticleFineSamplingPencilBeamEngine.m
+++ matRad/doseCalc/+DoseEngines/matRad_ParticleFineSamplingPencilBeamEngine.m
@@ -68,6 +68,14 @@ classdef matRad_ParticleFineSamplingPencilBeamEngine < DoseEngines.matRad_Partic
             ray.rotMat_system_T = beam.rotMat_system_T;
         end
 
+        function [currBixel] = getBixelIndicesOnRay(this,currBixel,currRay)
+            % In Finesampling we can not reduce the number of points to be
+            % computed that easily based on the rad depth, so we overload
+            % this method to just give all indices stored in the ray
+            currBixel.subRayIx = true(size(currRay.ix));
+            currBixel.ix = currRay.ix;
+        end
+
         % We override this function to get full lateral distances
         function ray = getRayGeometryFromBeam(this,ray,currBeam)
             lateralRayCutOff = this.getLateralDistanceFromDoseCutOffOnRay(ray);
