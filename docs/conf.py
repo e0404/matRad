2da1de674ea52c5bf4d22111211d080a5f2f0a54
Niklas Wahl <niklaswahl2011@gmail.com>
2026-03-25 22:42:02 +0100

    Update version number, license and changelog.

--- docs/conf.py
+++ docs/conf.py
@@ -45,8 +45,8 @@ project = "matRad"
 copyright = "2025, e0404"
 author = "e0404"
 
-version = "3.2.0"
-release = "3.2.0"
+version = "3.2.1"
+release = "3.2.1"
 
 html_theme = "sphinx_rtd_theme" #pip install sphinx-rtd-theme
 
