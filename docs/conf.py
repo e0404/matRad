95879a6c44d3894c0f4e2432e091e4185dd308da
AD\c657e <cheyenne.leize@gmail.com>
2026-05-29 10:14:30 +0200

    changed favicon to hat

--- docs/conf.py
+++ docs/conf.py
@@ -56,7 +56,7 @@ html_logo = "../matRad/gfx/matrad_logo.png"
 
 templates_path = ["_templates"]
 
-favicon_path = "_static/favicon_mR/"
+favicon_path = "_static/favicon_hat/"
 
 html_context = {
 	"fav": favicon_path,
